36549c54ed9f197468f01ce132006db4c25bc53d
Kenneth Reitz <me@kennethreitz.org>
2017-01-21 14:45:06 -0500

    update tests for refactor

--- test_pipenv.py
+++ test_pipenv.py
@@ -1,6 +1,6 @@
 import pytest
 
-import pipenv
+import pipenv.utils
 
 
 
@@ -8,27 +8,27 @@ def test_convert_deps_to_pip():
 
     # requests = '*'
     deps = {'requests': '*'}
-    deps = pipenv.convert_deps_to_pip(deps)
+    deps = pipenv.utils.convert_deps_to_pip(deps)
     assert deps[0] == 'requests'
 
     # requests = {}
     deps = {'requests': {}}
-    deps = pipenv.convert_deps_to_pip(deps)
+    deps = pipenv.utils.convert_deps_to_pip(deps)
     assert deps[0] == 'requests'
 
     # requests = { extras = ['socks'] }
     deps = {'requests': {'extras': ['socks']}}
-    deps = pipenv.convert_deps_to_pip(deps)
+    deps = pipenv.utils.convert_deps_to_pip(deps)
     assert deps[0] == 'requests[socks]'
 
     # Django = '>1.10'
     deps = {'django': '>1.10'}
-    deps = pipenv.convert_deps_to_pip(deps)
+    deps = pipenv.utils.convert_deps_to_pip(deps)
     assert deps[0] == 'django>1.10'
 
     # pinax = { git = 'git://github.com/pinax/pinax.git', ref = '1.4', editable = true }
     deps = {'pinax': {'git': 'git://github.com/pinax/pinax.git', 'ref': '1.4', 'editable': True}}
-    deps = pipenv.convert_deps_to_pip(deps)
+    deps = pipenv.utils.convert_deps_to_pip(deps)
     assert deps[0] == '-e git+git://github.com/pinax/pinax.git@1.4 --egg=pinax'
 
 
@@ -36,17 +36,17 @@ def test_convert_from_pip():
 
     # requests
     dep = 'requests'
-    dep = pipenv.convert_deps_from_pip(dep)
+    dep = pipenv.utils.convert_deps_from_pip(dep)
     assert dep == {'requests': '*'}
 
     # Django>1.10
     dep = 'Django>1.10'
-    dep = pipenv.convert_deps_from_pip(dep)
+    dep = pipenv.utils.convert_deps_from_pip(dep)
     assert dep == {'Django': '>1.10'}
 
     # requests[socks]
     dep = 'requests[socks]'
-    dep = pipenv.convert_deps_from_pip(dep)
+    dep = pipenv.utils.convert_deps_from_pip(dep)
     assert dep == {'requests': {'extras': ['socks']}}
 
     # TODO: -e git+git://github.com/pinax/pinax.git@1.4
