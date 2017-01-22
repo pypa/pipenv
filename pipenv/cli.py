7d2ef8c3102e73e2acee7d2928b95b8cc920af18
Kenneth Reitz <me@kennethreitz.org>
2017-01-21 19:43:03 -0500

    v0.1.6

--- pipenv/cli.py
+++ pipenv/cli.py
@@ -13,7 +13,7 @@ import _pipfile as pipfile
 from .project import Project
 from .utils import convert_deps_from_pip, convert_deps_to_pip
 
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 
 
 project = Project()
