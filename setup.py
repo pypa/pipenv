55ddc91172a23ba1630cd1a58cd2246ff1f09b09
Kenneth Reitz <me@kennethreitz.org>
2017-01-22 19:56:20 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -49,6 +49,18 @@ setup(
     install_requires=required,
     license='MIT',
     classifiers=(
-
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
     ),
 )
