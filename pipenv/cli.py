a679af22ad22d576827ded9903e02da794ebad28
Kenneth Reitz <me@kennethreitz.org>
2017-01-25 00:02:55 -0500

    easter egg

--- pipenv/cli.py
+++ pipenv/cli.py
@@ -474,7 +474,7 @@ def format_pip_output(out, r=None):
     return out
 
 def easter_egg(package_name):
-    if package_name in ['requests', 'maya', 'crayons', 'delegator.py']:
+    if package_name in ['requests', 'maya', 'crayons', 'delegator.py' 'records', 'tablib']:
         click.echo('P.S. You have excellent taste! ✨🍰✨')
 
 @click.group(invoke_without_command=True)
