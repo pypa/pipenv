013400ae2856c0f57c400f0bbfabd429e5daccaf
Kenneth Reitz <me@kennethreitz.org>
2017-01-24 21:34:44 -0500

    fix broken tests

--- test_pipenv.py
+++ test_pipenv.py
@@ -1,6 +1,5 @@
 import pytest
 
-from pipenv.cli import from_requirements_file
 import pipenv.utils
 
 
@@ -9,32 +8,32 @@ def test_convert_deps_to_pip():
 
     # requests = '*'
     deps = {'requests': '*'}
-    deps = pipenv.utils.convert_deps_to_pip(deps)
+    deps = pipenv.utils.convert_deps_to_pip(deps, r=False)
     assert deps[0] == 'requests'
 
     # requests = {}
     deps = {'requests': {}}
-    deps = pipenv.utils.convert_deps_to_pip(deps)
+    deps = pipenv.utils.convert_deps_to_pip(deps, r=False)
     assert deps[0] == 'requests'
 
     # requests = { extras = ['socks'] }
     deps = {'requests': {'extras': ['socks']}}
-    deps = pipenv.utils.convert_deps_to_pip(deps)
+    deps = pipenv.utils.convert_deps_to_pip(deps, r=False)
     assert deps[0] == 'requests[socks]'
 
     # Django = '>1.10'
     deps = {'django': '>1.10'}
-    deps = pipenv.utils.convert_deps_to_pip(deps)
+    deps = pipenv.utils.convert_deps_to_pip(deps, r=False)
     assert deps[0] == 'django>1.10'
 
     # pinax = { git = 'git://github.com/pinax/pinax.git', ref = '1.4', editable = true }
     deps = {'pinax': {'git': 'git://github.com/pinax/pinax.git', 'ref': '1.4', 'editable': True}}
-    deps = pipenv.utils.convert_deps_to_pip(deps)
+    deps = pipenv.utils.convert_deps_to_pip(deps, r=False)
     assert deps[0] == '-e git+git://github.com/pinax/pinax.git@1.4#egg=pinax'
 
     # pinax = { git = 'git://github.com/pinax/pinax.git', ref = '1.4'}
     deps = {'pinax': {'git': 'git://github.com/pinax/pinax.git', 'ref': '1.4'}}
-    deps = pipenv.utils.convert_deps_to_pip(deps)
+    deps = pipenv.utils.convert_deps_to_pip(deps, r=False)
     assert deps[0] == 'git+git://github.com/pinax/pinax.git@1.4#egg=pinax'
 
 def test_convert_from_pip():
@@ -58,25 +57,3 @@ def test_convert_from_pip():
     dep = pipenv.utils.convert_deps_from_pip(dep)
     assert dep == {u'MyProject': {u'svn': u'svn://svn.myproject.org/svn/MyProject', 'editable': True}}
 
-
-def test_install_from_requirements_file():
-
-    # requests
-    r = open('tests/requirements_requests.txt')
-    dep = from_requirements_file(r)
-    assert dep == ['requests']
-
-    # Django>1.10
-    r = open('tests/requirements_django.txt')
-    dep = from_requirements_file(r)
-    assert dep == ['Django>1.10']
-
-    # requests[sock]
-    r = open('tests/requirements_requests_socks.txt')
-    dep = from_requirements_file(r)
-    assert dep == ['requests[socks]']
-
-    # -e svn+svn://svn.myproject.org/svn/MyProject#egg=MyProject  # comment
-    r = open('tests/requirements_egg.txt')
-    dep = from_requirements_file(r)
-    assert dep == ['-e svn+svn://svn.myproject.org/svn/MyProject#egg=MyProject']
