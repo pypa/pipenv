bca81b97ad8aeeb6649742ba380ac068fa3705b0
Jace Browning <jacebrowning@gmail.com>
2017-01-26 16:57:46 -0500

    Prevent invalid shebangs with Homebrew Python
    Fixes https://github.com/kennethreitz/pipenv/issues/98

--- pipenv/cli.py
+++ pipenv/cli.py
@@ -440,6 +440,8 @@ def do_init(dev=False, requirements=False, skip_virtualenv=False, allow_global=F
 
 
 def pip_install(package_name=None, r=None, allow_global=False):
+    # Prevent invalid shebangs with Homebrew-installed Python: https://bugs.python.org/issue22490
+    os.environ.pop('__PYVENV_LAUNCHER__', None)
     if r:
         c = delegator.run('{0} install -r {1} --require-hashes -i {2}'.format(which_pip(allow_global=allow_global), r, project.source['url']))
     else:
