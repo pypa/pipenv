2cd8ed0d63f89a4009e9935065b104ccda896671
Nate Prewitt <Nate.Prewitt@gmail.com>
2017-01-28 22:34:36 -0700

    move __PYENV_LAUNCHER__ unset so it's only run once

--- pipenv/cli.py
+++ pipenv/cli.py
@@ -39,6 +39,9 @@ click_completion.init()
 # Disable warnings for Python 2.6.
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
+# Prevent invalid shebangs with Homebrew-installed Python: https://bugs.python.org/issue22490
+os.environ.pop('__PYVENV_LAUNCHER__', None)
+
 project = Project()
 
 
@@ -499,8 +502,6 @@ def do_init(dev=False, requirements=False, skip_virtualenv=False, allow_global=F
 
 
 def pip_install(package_name=None, r=None, allow_global=False):
-    # Prevent invalid shebangs with Homebrew-installed Python: https://bugs.python.org/issue22490
-    os.environ.pop('__PYVENV_LAUNCHER__', None)
     if r:
         c = delegator.run('{0} install -r {1} --require-hashes -i {2}'.format(which_pip(allow_global=allow_global), r, project.source['url']))
     else:
