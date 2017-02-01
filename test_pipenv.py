65dd2c323dfdbe67e5ef7292928c5500aee7e327
Natalie Pendragon <natalie.pendragon@gmail.com>
2017-01-31 20:46:45 -0500

    Improve dashed version parsing
    Instead of chopping off the ends of versions containing dashes, this
    will keep them intact. Also add some tests for the
    parse_download_fname function.

--- test_pipenv.py
+++ test_pipenv.py
@@ -1,6 +1,34 @@
 import pytest
 
+from pipenv.cli import parse_download_fname
 import pipenv.utils
+
+
+def test_parse_download_fname():
+
+    fname = 'functools32-3.2.3-2.zip'
+    version = parse_download_fname(fname)
+    assert version == '3.2.3-2'
+
+    fname = 'functools32-3.2.3-blah.zip'
+    version = parse_download_fname(fname)
+    assert version == '3.2.3-blah'
+
+    fname = 'functools32-3.2.3.zip'
+    version = parse_download_fname(fname)
+    assert version == '3.2.3'
+
+    fname = 'colorama-0.3.7-py2.py3-none-any.whl'
+    version = parse_download_fname(fname)
+    assert version == '0.3.7'
+
+    fname = 'colorama-0.3.7-2-py2.py3-none-any.whl'
+    version = parse_download_fname(fname)
+    assert version == '0.3.7-2'
+
+    fname = 'click-completion-0.2.1.tar.gz'
+    version = parse_download_fname(fname)
+    assert version == '0.2.1'
 
 
 def test_convert_deps_to_pip():
