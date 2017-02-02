fdffd9f086d180d65605c9828d3520ac0a9400ac
Kenneth Reitz <me@kennethreitz.org>
2017-02-01 20:42:00 -0500

    magic sauce

--- pipenv/environments.py
+++ pipenv/environments.py
@@ -11,3 +11,7 @@ PIPENV_COLORBLIND = os.environ.get('PIPENV_COLORBLIND')
 
 # User-configuraable max-depth for Pipfile searching.
 PIPENV_MAX_DEPTH = int(os.environ.get('PIPENV_MAX_DEPTH', '3'))
+
+# Use shell compatibility mode when using venv in project mode.
+if PIPENV_VENV_IN_PROJECT:
+    PIPENV_SHELL_COMPAT = True
