4f9d27aba64c74e744cff0e368bccc69689ce452
Nate Prewitt <Nate.Prewitt@gmail.com>
2017-02-11 15:34:40 -0700

    removing unneeded pew test

--- tests/test_project.py
+++ tests/test_project.py
@@ -10,10 +10,6 @@ class TestProject():
         assert proj.pipfile_exists
         assert proj.virtualenv_exists
 
-    def test_pew_by_default(self):
-        proj = pipenv.project.Project()
-        assert proj.virtualenv_location.endswith('.local/share/virtualenvs/pipenv')
-
     def test_proper_names(self):
         proj = pipenv.project.Project()
         assert proj.virtualenv_location in proj.proper_names_location
