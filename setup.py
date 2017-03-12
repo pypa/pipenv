70b93ce8391bc7b1e7acc3f06e99c79833e00c8b
Kenneth Reitz <me@kennethreitz.org>
2017-03-11 23:40:13 -0500

    updates to vendoring

--- setup.py
+++ setup.py
@@ -26,10 +26,6 @@ required = [
     'pip'
 ]
 
-# Backport required for earlier versions of Python.
-if sys.version_info < (3, 3):
-    required.append('backports.shutil_get_terminal_size')
-
 if sys.version_info < (2, 7):
     required.append('requests[security]')
     required.append('ordereddict')
@@ -42,7 +38,22 @@ setup(
     author='Kenneth Reitz',
     author_email='me@kennethreitz.org',
     url='https://github.com/kennethreitz/pipenv',
-    packages=['pipenv'],
+    packages=[
+        'pipenv', 'pipenv.vendor',
+        'pipenv.vendor.backports.shutil_get_terminal_size',
+        'pipenv.vendor.blindspin', 'pipenv.vendor.click',
+        'pipenv.vendor.colorama', 'pipenv.vendor.pexpect',
+        'pipenv.vendor.pipfile', 'pipenv.vendor.psutil',
+        'pipenv.vendor.ptyprocess', 'pipenv.vendor.requests',
+        'pipenv.vendor.requests.packages',
+        'pipenv.vendor.requests.packages.chardet',
+        'pipenv.vendor.requests.packages.urllib3',
+        'pipenv.vendor.requests.packages.urllib3.contrib',
+        'pipenv.vendor.requests.packages.urllib3.packages',
+        'pipenv.vendor.requests.packages.urllib3.packages.backports',
+        'pipenv.vendor.requests.packages.urllib3.packages.ssl_match_hostname',
+        'pipenv.vendor.requests.packages.idna', 'pipenv.vendor.requests.packages.urllib3.util',
+        'pipenv.vendor.requirements', 'pipenv.vendor.shutilwhich'],
     entry_points={
         'console_scripts': ['pipenv=pipenv:cli'],
     },
