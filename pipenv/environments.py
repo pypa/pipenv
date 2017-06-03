5ef5bb0f79a688b0ae4269ed1451f2e3fa2bb7c8
Kenneth Reitz <me@kennethreitz.org>
2017-06-03 19:07:05 -0400

    switch to disable virtualenvs

--- pipenv/environments.py
+++ pipenv/environments.py
@@ -26,7 +26,7 @@ PIPENV_MAX_DEPTH = int(os.environ.get('PIPENV_MAX_DEPTH', '3')) + 1
 PIPENV_DEFAULT_THREE = os.environ.get('PIPENV_DEFAULT_THREE')
 
 # Tells Pipenv to use the virtualenv-provided pip instead.
-PIPENV_USE_SYSTEM = os.environ.get('VIRTUAL_ENV')
+PIPENV_USE_SYSTEM = os.environ.get('VIRTUAL_ENV') if not 'PIPENV_IGNORE_VIRTUALENVS' in os.environ else False
 
 # Use shell compatibility mode when using venv in project mode.
 if PIPENV_VENV_IN_PROJECT:
