3bf81f5b51b7188362aaff87a1405272ac2b3f6f
Kenneth Reitz <me@kennethreitz.org>
2017-08-31 12:34:33 -0400

    ch ch changes!
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@
 import codecs
 import os
 import sys
+from shutil import rmtree
 
-from setuptools import setup
+from setuptools import setup, Command
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -29,6 +30,40 @@ required = [
 if sys.version_info < (2, 7):
     required.append('requests[security]')
     required.append('ordereddict')
+
+
+class PublishCommand(Command):
+    """Support setup.py publish."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except FileNotFoundError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
 
 setup(
     name='pipenv',
