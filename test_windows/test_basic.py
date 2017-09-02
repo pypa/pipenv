4ba1f1285cffda7a1cfaac378d90025cf6a4de3a
Kenneth Reitz <me@kennethreitz.org>
2017-09-01 22:37:30 -0400

    basic windows tests
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- test_windows/test_basic.py
+++ test_windows/test_basic.py
@@ -1,3 +1,15 @@
+import delegator
+
 class TestPipenv():
+
     def test_existience(self):
         assert True
+
+    def test_install(self):
+        c = delegator.run('pipenv install')
+        assert c.return_code == 0
+
+    def test_lock(self):
+        c = delegator.run('pipenv lock')
+        assert c.return_code == 0
+
