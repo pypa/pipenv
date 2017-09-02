fe74663ba8ad1c5e668efd18f41f28d971279d9b
Kenneth Reitz <me@kennethreitz.org>
2017-09-01 22:33:18 -0400

    better reporting on windows
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- pipenv/cli.py
+++ pipenv/cli.py
@@ -380,11 +380,13 @@ def do_create_virtualenv(three=None, python=None):
     if python:
         click.echo('{0} {1} {2}'.format(crayons.yellow('Using'), crayons.red(python), crayons.yellow('to create virtualenv...')))
     else:
-        if os.name == 'nt':
-            click.echo('{0} If you are running on Windows, you should use the {1} option.'.format(crayons.red('Warning!'), crayons.green('--python')))
         if three is False:
+            if os.name == 'nt':
+                click.echo('{0} If you are running on Windows, you should use the {1} option, instead.'.format(crayons.red('Warning!'), crayons.green('--python')))
             python = 'python2'
         elif three is True:
+            if os.name == 'nt':
+                click.echo('{0} If you are running on Windows, you should use the {1} option, instead.'.format(crayons.red('Warning!'), crayons.green('--python')))
             python = 'python3'
     if python:
         cmd = cmd + ['-p', python]
