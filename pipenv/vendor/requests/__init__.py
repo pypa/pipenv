fb390662afa0084c38afd17ad399804b3a8ad145
Kenneth Reitz <me@kennethreitz.org>
2017-09-02 17:35:24 -0400

    speed up requests
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- pipenv/vendor/requests/__init__.py
+++ pipenv/vendor/requests/__init__.py
@@ -49,8 +49,10 @@ __copyright__ = 'Copyright 2016 Kenneth Reitz'
 
 # Attempt to enable urllib3's SNI support, if possible
 try:
-    from .packages.urllib3.contrib import pyopenssl
-    pyopenssl.inject_into_urllib3()
+    # Commented out, for speed-ups.
+    # from .packages.urllib3.contrib import pyopenssl
+    # pyopenssl.inject_into_urllib3()
+    pass
 except ImportError:
     pass
 
