47604c644bf18bbbb39800833fcf0dd96526f1eb
Kenneth Reitz <me@kennethreitz.org>
2017-09-01 21:33:39 -0400

    fix setup.py
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- setup.py
+++ setup.py
@@ -101,7 +101,6 @@ setup(
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
