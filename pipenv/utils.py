b3f0b628a17b0777fb4ccf748918408e638fafaa
Kenneth Reitz <me@kennethreitz.org>
2017-09-15 12:00:05 -0400

    str python
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- pipenv/utils.py
+++ pipenv/utils.py
@@ -55,7 +55,7 @@ class HackedPythonVersion(object):
 
     def __enter__(self):
         if self.python:
-            os.environ['PIP_PYTHON_VERSION'] = self.python
+            os.environ['PIP_PYTHON_VERSION'] = str(self.python)
 
     def __exit__(self, *args):
         # Restore original Python version information.
