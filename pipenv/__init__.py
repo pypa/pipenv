7b4286d61898ca9cc18391adc9181166af9a47cd
Kenneth Reitz <me@kennethreitz.org>
2017-09-14 20:43:34 -0400

    patched directory
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- pipenv/__init__.py
+++ pipenv/__init__.py
@@ -10,6 +10,10 @@ import sys
 v_path = os.path.sep.join([os.path.dirname(os.path.realpath(__file__)), 'vendor'])
 sys.path.insert(0, v_path)
 
+# Inject patched directory into system path.
+v_path = os.path.sep.join([os.path.dirname(os.path.realpath(__file__)), 'patched'])
+sys.path.insert(0, v_path)
+
 from .cli import cli
 
 if __name__ == '__main__':
