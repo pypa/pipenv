21b2d630ad0a557f92b63469fe04ea7383d8b5df
Kenneth Reitz <me@kennethreitz.org>
2017-09-19 11:49:37 -0400

    use absolute paths
    maybe fix #594?
    
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- pipenv/__init__.py
+++ pipenv/__init__.py
@@ -7,11 +7,11 @@ import os
 import sys
 
 # Inject vendored directory into system path.
-v_path = os.path.sep.join([os.path.dirname(os.path.realpath(__file__)), 'vendor'])
+v_path = os.path.abspath(os.path.sep.join([os.path.dirname(os.path.realpath(__file__)), 'vendor']))
 sys.path.insert(0, v_path)
 
 # Inject patched directory into system path.
-v_path = os.path.sep.join([os.path.dirname(os.path.realpath(__file__)), 'patched'])
+v_path = os.path.abspath(os.path.sep.join([os.path.dirname(os.path.realpath(__file__)), 'patched']))
 sys.path.insert(0, v_path)
 
 from .cli import cli
