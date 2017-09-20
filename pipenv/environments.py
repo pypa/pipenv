85f8c20e1537f2a38856832138d5e832e49958e0
Kenneth Reitz <me@kennethreitz.org>
2017-09-20 12:46:56 -0400

    cast all environment keys to bools
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- pipenv/environments.py
+++ pipenv/environments.py
@@ -7,22 +7,22 @@ os.environ.pop('__PYVENV_LAUNCHER__', None)
 
 
 # Shell compatibility mode, for mis-configured shells.
-PIPENV_SHELL_COMPAT = os.environ.get('PIPENV_SHELL_COMPAT')
+PIPENV_SHELL_COMPAT = bool(os.environ.get('PIPENV_SHELL_COMPAT'))
 
 # Create the virtualenv in the project, isntead of with pew.
-PIPENV_VENV_IN_PROJECT = os.environ.get('PIPENV_VENV_IN_PROJECT')
+PIPENV_VENV_IN_PROJECT = bool(os.environ.get('PIPENV_VENV_IN_PROJECT'))
 
 # No color mode, for unfun people.
-PIPENV_COLORBLIND = os.environ.get('PIPENV_COLORBLIND')
+PIPENV_COLORBLIND = bool(os.environ.get('PIPENV_COLORBLIND'))
 
 # Disable spinner for better test and deploy logs (for the unworthy).
-PIPENV_NOSPIN = os.environ.get('PIPENV_NOSPIN')
+PIPENV_NOSPIN = bool(os.environ.get('PIPENV_NOSPIN'))
 
 # Tells pipenv to not try to install -e dependencies to get their dependency information.
-PIPENV_DONT_EAT_EDITABLES = os.environ.get('PIPENV_DONT_EAT_EDITABLES')
+PIPENV_DONT_EAT_EDITABLES = bool(os.environ.get('PIPENV_DONT_EAT_EDITABLES'))
 
 # Tell Pipenv to default to yes at all prompts.
-PIPENV_YES = os.environ.get('PIPENV_YES')
+PIPENV_YES = bool(os.environ.get('PIPENV_YES'))
 
 # User-configuraable max-depth for Pipfile searching.
 # Note: +1 because of a temporary bug in Pipenv.
@@ -32,7 +32,7 @@ PIPENV_MAX_DEPTH = int(os.environ.get('PIPENV_MAX_DEPTH', '3')) + 1
 PIPENV_USE_SYSTEM = False
 if 'PIPENV_ACTIVE' not in os.environ:
     if 'PIPENV_IGNORE_VIRTUALENVS' not in os.environ:
-        PIPENV_USE_SYSTEM = os.environ.get('VIRTUAL_ENV')
+        PIPENV_USE_SYSTEM = bool(os.environ.get('VIRTUAL_ENV'))
 
 # Tells Pipenv to use hashing mode.
 PIPENV_USE_HASHES = True
@@ -51,7 +51,7 @@ if os.name == 'nt':
 if 'CI' in os.environ:
     PIPENV_NOSPIN = True
 
-PIPENV_HIDE_EMOJIS = os.environ.get('PIPENV_HIDE_EMOJIS')
+PIPENV_HIDE_EMOJIS = bool(os.environ.get('PIPENV_HIDE_EMOJIS'))
 if os.name == 'nt':
     PIPENV_HIDE_EMOJIS = True
 
