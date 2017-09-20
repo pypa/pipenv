b68f306eb089f06079025f0247a1b88a47d15d12
Kenneth Reitz <me@kennethreitz.org>
2017-09-20 12:17:25 -0400

    automatically push git tags
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- setup.py
+++ setup.py
@@ -65,6 +65,10 @@ class UploadCommand(Command):
         self.status('Uploading the package to PyPi via Twine…')
         os.system('twine upload dist/*')
 
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
         sys.exit()
 
 
