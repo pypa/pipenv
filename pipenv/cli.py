3633a8e7e837c275adaac842a27bf86934346483
Kenneth Reitz <me@kennethreitz.org>
2017-09-21 14:00:00 -0400

    only do saftey on python 3
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- pipenv/cli.py
+++ pipenv/cli.py
@@ -1924,38 +1924,40 @@ def check(three=None, python=False):
     else:
         click.echo(crayons.green('Passed!'))
 
-    click.echo(
-        crayons.white(u'Checking installed package safety…', bold=True)
-    )
-
-    path = pep508checker.__file__.rstrip('cdo')
-    path = os.sep.join(__file__.split(os.sep)[:-1] + ['vendor', 'safety.zip'])
-
-    c = delegator.run('"{0}" {1} check --json'.format(which('python'), shellquote(path)))
-    try:
-        results = simplejson.loads(c.out)
-    except ValueError:
-        click.echo('An error occured:', err=True)
-        click.echo(c.err, err=True)
-        sys.exit(1)
-
-    for (package, resolved, installed, description, vuln) in results:
+    # This part only works on Python 3.
+    if (python_version(which('python')) or '').startswith('3'):
         click.echo(
-            '{0}: {1} {2} resolved ({3} installed)!'.format(
-                crayons.white(vuln, bold=True),
-                crayons.green(package),
-                crayons.red(resolved, bold=False),
-                crayons.red(installed, bold=True)
-            )
+            crayons.white(u'Checking installed package safety…', bold=True)
         )
 
-        click.echo('{0}'.format(description))
-        click.echo()
+        path = pep508checker.__file__.rstrip('cdo')
+        path = os.sep.join(__file__.split(os.sep)[:-1] + ['vendor', 'safety.zip'])
 
-    if not results:
-        click.echo(crayons.green('All good!'))
-    else:
-        sys.exit(1)
+        c = delegator.run('"{0}" {1} check --json'.format(which('python'), shellquote(path)))
+        try:
+            results = simplejson.loads(c.out)
+        except ValueError:
+            click.echo('An error occured:', err=True)
+            click.echo(c.err, err=True)
+            sys.exit(1)
+
+        for (package, resolved, installed, description, vuln) in results:
+            click.echo(
+                '{0}: {1} {2} resolved ({3} installed)!'.format(
+                    crayons.white(vuln, bold=True),
+                    crayons.green(package),
+                    crayons.red(resolved, bold=False),
+                    crayons.red(installed, bold=True)
+                )
+            )
+
+            click.echo('{0}'.format(description))
+            click.echo()
+
+        if not results:
+            click.echo(crayons.green('All good!'))
+        else:
+            sys.exit(1)
 
 
 
