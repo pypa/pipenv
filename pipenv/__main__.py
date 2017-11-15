ffe7612af1dde3afc951c3bfeb0b91331b86057b
Maximilian Remming <maxremming@gmail.com>
2017-11-15 20:24:34 +0200

    Suggested improvements from #953
    The main issue with slow startup times seems to be that `from .cli import cli` takes quite long, this commit moves these imports to conditional blocks so they're run as late as possible, and not at all if not needed.

--- pipenv/__main__.py
+++ pipenv/__main__.py
@@ -1,4 +1,10 @@
-from .cli import cli
+import os
 
 if __name__ == '__main__':
-    cli()
+    pipenv_complete = os.environ.get("_PIPENV_COMPLETE")
+    if pipenv_complete:
+        import click_completion
+        click_completion._shellcomplete(None, "pipenv")
+    else:
+        from .cli import cli
+        cli()
