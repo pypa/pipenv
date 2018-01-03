dac4ba76b8fb848fd887a7c660950120d3ae71bf
Tzu-ping Chung <uranusjr@gmail.com>
2018-01-03 15:06:41 +0800

    Use os.path.realpath to fix test on macOS
    On macOS /var is a symlink to /private/var. Pew returns the latter, but
    the test expected the former.

--- tests/test_pipenv.py
+++ tests/test_pipenv.py
@@ -267,7 +267,7 @@ records = "*"
             assert c.return_code != 0
             c = p.pipenv('run python -c "import tablib"')
             assert c.return_code == 0
-                
+
 
     @pytest.mark.run
     @pytest.mark.uninstall
@@ -318,10 +318,10 @@ records = "*"
         with PipenvInstance() as p:
             c = p.pipenv('install --dev requests pytest')
             assert c.return_code == 0
-            
+
             c = p.pipenv('install tpfd')
             assert c.return_code == 0
-            
+
             assert 'tpfd' in p.pipfile['packages']
             assert 'requests' in p.pipfile['dev-packages']
             assert 'pytest' in p.pipfile['dev-packages']
@@ -342,7 +342,7 @@ records = "*"
 
             c = p.pipenv('run python -m requests.help')
             assert c.return_code > 0
-            
+
             c = p.pipenv('run python -c "import tpfd"')
             assert c.return_code == 0
 
@@ -608,7 +608,7 @@ requests = {version = "*", os_name = "== 'splashwear'"}
             assert 'tablib' in p.lockfile['default']
             assert 'git' in p.lockfile['default']['tablib']
             assert p.lockfile['default']['tablib']['git'] == 'git://github.com/kennethreitz/tablib.git'
-            assert 'ref' in p.lockfile['default']['tablib']            
+            assert 'ref' in p.lockfile['default']['tablib']
 
     @pytest.mark.run
     @pytest.mark.alt
@@ -681,8 +681,8 @@ requests = {version = "*"}
                 c = delegator.run('pew dir .venv')
                 # Compare pew's virtualenv path to what we expect
                 venv_path = get_windows_path(project.project_directory, '.venv')
-                # os.path.normpath will normalize slashes
-                assert venv_path == os.path.normpath(c.out.strip())
+                # normpath normalizes slashes, realpath follows symlinks.
+                assert venv_path == os.path.realpath(os.path.normpath(c.out.strip()))
                 # Have pew run 'pip freeze' in the virtualenv
                 # This is functionally the same as spawning a subshell
                 # If we can do this we can theoretically amke a subshell
@@ -901,7 +901,7 @@ maya = "*"
     def test_resolve_system_python_no_virtualenv(self):
         """Ensure we don't error out when we are in a folder off of / and doing an install using --system,
         which used to cause the resolver and PIP_PYTHON_PATH to point at /bin/python
-        
+
         Sample dockerfile:
         FROM python:3.6-alpine3.6
 
