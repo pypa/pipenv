pypa/pipenv

commit b411d20bd185b9c8e3c8300977d6ead422276ccb

Fix __delitem__ in prettytoml's table element
Two bugs involved here.

First of all, __delitem__ uses _find_following_newline to find where the
line to delete ends, which in turns looks for the next closest newline
element. But if the previous line ends with a comment element (which
implies a newline), it will also be deleted. This would be a problem if
the comment is inline.

foo=1  # comment
bar=2

In this minimal example, the elements in play are

AtomicElement('foo')
PunctuationElement('=')
AtomicElement(1)
WhitespaceElement('  ')
CommentElement('# comment')
AtomicElement('bar')
PunctuationElement('=')
AtomicElement(2)

When deleting "foo", it starts from AtomicElement('foo') and looks for
the next newline, but does not consider CommentElement, ending up
deleting all elements. This fix replaces _find_following_newline with
_find_following_line_terminator, which does consider both newline and
comment elements.

Also there is a logic error in _find_following_line_terminator that
prevents it from even working. The two -inf/inf clauses should serve as
guards when either or both indexes are infinity, but they were
flip-flopped and not working at all. This change doesn't seem to affect
existing tests (at least not breaking them more than they already were).

Fix #1535.

pipenv/patched/prettytoml/elements/traversal/__init__.py
@@ -76,9 +76,9 @@ class TraversalMixin:
         following_comment = self._find_following_comment(index)
         following_newline = self._find_following_newline(index)
 
-        if following_comment == float('-inf'):
+        if following_comment == float('inf'):
             return following_newline
-        if following_newline == float('inf'):
+        if following_newline == float('-inf'):
             return following_comment
 
         if following_newline < following_comment:

pipenv/patched/prettytoml/elements/table.py
@@ -105,7 +105,7 @@ class TableElement(abstracttable.AbstractTable):
         preceding_newline = self._find_preceding_newline(begin)
         if preceding_newline >= 0:
             begin = preceding_newline
-        end = self._find_following_newline(begin)
+        end = self._find_following_line_terminator(begin)
         if end < 0:
             end = len(tuple(self._sub_elements))
         self._sub_elements = self.sub_elements[:begin] + self.sub_elements[end:]

commit b70c2275e2d5e9eb38ff1f5437fcde91b4954d1a

Fix problems caused by b411d20
Also add the test case from #1536 so we can be sure things stay correct.

pipenv/patched/prettytoml/elements/traversal/__init__.py
@@ -76,7 +76,7 @@ class TraversalMixin:
         following_comment = self._find_following_comment(index)
         following_newline = self._find_following_newline(index)
 
-        if following_comment == float('inf'):
+        if following_comment == float('-inf'):
             return following_newline
         if following_newline == float('-inf'):
             return following_comment

tests/test_vendor.py
@@ -1,0 +1,31 @@
+# Make sure we use the patched packages.
+import pipenv   # noqa
+
+from prettytoml import lexer
+from prettytoml.elements.atomic import AtomicElement
+from prettytoml.elements.metadata import (
+    WhitespaceElement, PunctuationElement, CommentElement,
+)
+from prettytoml.elements.table import TableElement
+
+
+def test_table():
+
+    initial_toml = """id=42 # My id\nage=14"""
+    tokens = tuple(lexer.tokenize(initial_toml))
+    table = TableElement([
+        AtomicElement(tokens[0:1]),
+        PunctuationElement(tokens[1:2]),
+        AtomicElement(tokens[2:3]),
+        WhitespaceElement(tokens[3:4]),
+        CommentElement(tokens[4:6]),
+
+        AtomicElement(tokens[6:7]),
+        PunctuationElement(tokens[7:8]),
+        AtomicElement(tokens[8:9]),
+    ])
+
+    assert set(table.items()) == {('id', 42), ('age', 14)}
+
+    del table['id']
+    assert set(table.items()) == {('age', 14)}