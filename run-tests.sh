d0e2e218f9c036cbc8aded40109c7bc37a37ce1e
Kenneth Reitz <me@kennethreitz.org>
2018-03-04 16:31:07 -0500

    see if that works
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- run-tests.sh
+++ run-tests.sh
@@ -3,8 +3,15 @@
 PYPI_VENDOR_DIR="$(pwd)/tests/pypi/"
 export PYPI_VENDOR_DIR
 
+
 if [[ ! "$TEST_SUITE" ]]; then
 	TEST_SUITE = ""
 fi
+
+RAM_DISK="/media/ramdisk"
+export RAM_DISK
+
+sudo mkdir -p "$RAM_DISK"
+sudo mount -t tmpfs -o size=2048M tmpfs "$RAM_DISK"
 
 pytest -n 8 tests -m "$TEST_SUITE"
