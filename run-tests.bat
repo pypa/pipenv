24edcc8ee8c80c324fae1f97aec7aa5605e385dd
Kenneth Reitz <me@kennethreitz.org>
2018-03-05 14:46:46 -0500

    try concurrency again
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- run-tests.bat
+++ run-tests.bat
@@ -2,4 +2,4 @@ virtualenv .venv
 .venv\Scripts\pip install -e . --upgrade --upgrade-strategy=only-if-needed
 .venv\Scripts\pipenv install --dev
 
-SET PYPI_VENDOR_DIR=".\tests\pypi\" && .venv\Scripts\pipenv run pytest -v tests --tap-stream
+SET PYPI_VENDOR_DIR=".\tests\pypi\" && .venv\Scripts\pipenv run pytest -n auto -v tests --tap-stream
