44eeffdcdc80d76e7dab70b57aea761e79ae812e
Kenneth Reitz <me@kennethreitz.org>
2018-03-05 13:20:11 -0500

    no concucurrency for windows
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- run-tests.bat
+++ run-tests.bat
@@ -3,4 +3,4 @@ virtualenv .venv
 .venv\Scripts\pipenv install --dev
 
 
-.venv\Scripts\pipenv run pytest -n auto --boxed -v tests --tap-stream | tee results.tap
+.venv\Scripts\pipenv run pytest -v tests --tap-stream | tee results.tap
