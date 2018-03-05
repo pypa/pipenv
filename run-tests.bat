bfdf7d51fec2c89b399169cba92d71a60eb87ecc
Kenneth Reitz <me@kennethreitz.org>
2018-03-05 14:27:26 -0500

    set pypi directory
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- run-tests.bat
+++ run-tests.bat
@@ -2,5 +2,6 @@ virtualenv .venv
 .venv\Scripts\pip install -e . --upgrade --upgrade-strategy=only-if-needed
 .venv\Scripts\pipenv install --dev
 
+SET PYPI_VENDOR_DIR '.\tests\pypi\'
 
 .venv\Scripts\pipenv run pytest -v tests -m "windows or cli" --tap-stream
