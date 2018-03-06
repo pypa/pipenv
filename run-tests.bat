ca391fc20ccff507a6c14fbfceabf368c22dd868
Kenneth Reitz <me@kennethreitz.org>
2018-03-06 10:40:20 -0500

    try like this
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>
    
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- run-tests.bat
+++ run-tests.bat
@@ -5,4 +5,4 @@ virtualenv R:\.venv
 R:\.venv\Scripts\pip install -e . --upgrade --upgrade-strategy=only-if-needed
 R:\.venv\Scripts\pipenv install --dev
 
-SET RAM_DISK="R:" && SET PYPI_VENDOR_DIR=".\tests\pypi\" && R:\.venv\Scripts\pipenv run pytest -n auto -v tests --tap-stream
+SET RAM_DISK=R: && SET PYPI_VENDOR_DIR=".\tests\pypi\" && R:\.venv\Scripts\pipenv run pytest -n auto -v tests --tap-stream
