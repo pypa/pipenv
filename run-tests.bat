b81a6d9fda23828ac9cccc12c7b7c9839221edde
Kenneth Reitz <me@kennethreitz.org>
2018-03-06 11:11:24 -0500

    no file base
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- run-tests.bat
+++ run-tests.bat
@@ -1,4 +1,4 @@
-rem imdisk -P -a -s 2104515b -m R: -f C:\my_disk.ima -p "/FS:NTFS /C /Y"
+rem imdisk -P -a -s 2104515b -m R:-p "/FS:NTFS /C /Y"
 
 virtualenv R:\.venv
 R:\.venv\Scripts\pip install -e . --upgrade --upgrade-strategy=only-if-needed
