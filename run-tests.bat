de356753e06505bf4de8870ad030a5da5a26d12a
Kenneth Reitz <me@kennethreitz.org>
2018-03-06 11:18:54 -0500

    report.tap
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- run-tests.bat
+++ run-tests.bat
@@ -1,8 +1,7 @@
-rem imdisk -P -a -s 4804515b -m R:-p "/FS:NTFS /C /Y"
+rem imdisk -P -a -s 9604515b -m R:-p "/FS:NTFS /Y"
 
 virtualenv R:\.venv
 R:\.venv\Scripts\pip install -e . --upgrade --upgrade-strategy=only-if-needed
 R:\.venv\Scripts\pipenv install --dev
 
-SET RAM_DISK=R:&& SET PYPI_VENDOR_DIR=".\tests\pypi\" && R:\.venv\Scripts\pipenv run pytest -n auto -v tests
-rem --tap-stream
+SET RAM_DISK=R:&& SET PYPI_VENDOR_DIR=".\tests\pypi\" && R:\.venv\Scripts\pipenv run pytest -n auto -v tests --tap-stream > report.tap
