51d8d3c67bfd2adc7d4a552e3608d9e181393133
Kenneth Reitz <me@kennethreitz.org>
2018-03-06 11:02:40 -0500

    cleanup
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- run-tests.bat
+++ run-tests.bat
@@ -1,6 +1,5 @@
 rem imdisk -P -a -s 2104515b -m R: -f C:\my_disk.ima -p "/FS:NTFS /C /Y"
 
-
 virtualenv R:\.venv
 R:\.venv\Scripts\pip install -e . --upgrade --upgrade-strategy=only-if-needed
 R:\.venv\Scripts\pipenv install --dev
