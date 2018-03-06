146dc0f4672c530fcf6c0a269ea22b4ba3bff5b1
Kenneth Reitz <me@kennethreitz.org>
2018-03-06 10:27:53 -0500

    ram disk for windows
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- run-tests.bat
+++ run-tests.bat
@@ -1,8 +1,8 @@
-imdisk -a -s 2104515b -m R: -f C:\my_disk.ima -p "/FS:NTFS /C /Y"
-set RAM_DISK="R:\"
+rem imdisk -a -s 2104515b -m R: -f C:\my_disk.ima -p "/FS:NTFS /C /Y"
+set RAM_DISK="R:"
 
 virtualenv R:\.venv
 R:\.venv\Scripts\pip install -e . --upgrade --upgrade-strategy=only-if-needed
 R:\.venv\Scripts\pipenv install --dev
 
-SET RAM_DISK="R:\" && SET PYPI_VENDOR_DIR=".\tests\pypi\" && R:\.venv\Scripts\pipenv run pytest -n auto -v tests --tap-stream
+SET RAM_DISK="R:" && SET PYPI_VENDOR_DIR=".\tests\pypi\" && R:\.venv\Scripts\pipenv run pytest -n auto -v tests --tap-stream
