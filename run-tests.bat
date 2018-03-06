5570322e7397060d899c7984158fb9dec4e56699
Kenneth Reitz <me@kennethreitz.org>
2018-03-06 10:50:07 -0500

    getting drastic
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- run-tests.bat
+++ run-tests.bat
@@ -1,8 +1,9 @@
 rem imdisk -a -s 2104515b -m R: -f C:\my_disk.ima -p "/FS:NTFS /C /Y"
 
-virtualenv R:\.venv
-R:\.venv\Scripts\pip install -e . --upgrade --upgrade-strategy=only-if-needed
-R:\.venv\Scripts\pipenv install --dev
+
+rem virtualenv R:\.venv
+rem R:\.venv\Scripts\pip install -e . --upgrade --upgrade-strategy=only-if-needed
+rem R:\.venv\Scripts\pipenv install --dev
 
 SET RAM_DISK=R:&& SET PYPI_VENDOR_DIR=".\tests\pypi\" && R:\.venv\Scripts\pipenv run pytest -n auto -v tests
 rem --tap-stream
