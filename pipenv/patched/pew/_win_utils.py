46b6ab30e3da25ff2ce8b732e34d2854cb11f831
Dan Ryan <dan@danryan.co>
2018-03-08 13:40:08 -0500

    Don't assume grandparent process is the shell
    - Windows only (pew implementation bug)
    - Traverse process tree until we find a known shell
      like cmd or powershell currently

--- pipenv/patched/pew/_win_utils.py
+++ pipenv/patched/pew/_win_utils.py
@@ -105,3 +105,22 @@ def get_grandparent_process(pid=None):
     parent = processes[ppid]
     grandparent = processes[parent['parent_pid']]
     return grandparent['executable']
+
+
+def get_shell(pid=None):
+    """Get the shell that the supplied pid or os.getpid() is running in.
+    """
+    if not pid:
+        pid = os.getpid()
+    processes = get_all_processes()
+
+    def check_parent(pid, lvl=0):
+        ppid = processes[pid].get('parent_pid')
+        if ppid and processes[ppid]['executable'].lower().rsplit('.', 1)[0] in ['cmd', 'powershell']:
+            return processes[ppid]['executable']
+        if lvl >= 4:
+            return
+        return check_parent(ppid, lvl=lvl+1)
+    if processes[pid]['executable'].lower().rsplit('.', 1)[-1] in ['cmd', 'powershell']:
+        return processes[pid]['executable']
+    return check_parent(pid)
