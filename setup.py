61edff204c81cef65275ab0b237445837e5ce80f
Kenneth Reitz <me@kennethreitz.org>
2018-03-12 17:27:53 -0400

    remove concurrent.futures
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- setup.py
+++ setup.py
@@ -55,8 +55,6 @@ class DebCommand(Command):
             self.status('Removing previous builds…')
             rmtree(os.path.join(here, 'deb_dist'))
 
-            # Remove concurrent27, at it causes issues with compilation.
-            rmtree(os.path.join(here, 'pipenv', 'vendor', 'concurrent27'))
         except FileNotFoundError:
             pass
         self.status(u'Creating debian mainfest…')
