66c7bad3d51dc270ea0463561ddd444b7ffbd6e8
Kenneth Reitz <me@kennethreitz.org>
2018-03-14 09:43:28 -0400

    hack

--- pipenv/__init__.py
+++ pipenv/__init__.py
@@ -16,6 +16,13 @@ sys.path.insert(0, PIPENV_VENDOR)
 # Inject patched directory into system path.
 sys.path.insert(0, PIPENV_PATCHED)
 
+# Hack to make things work better.
+try:
+    if 'concurrency' in sys.modules:
+        del sys.modules['concurrency']
+except Exception:
+    pass
+
 from .cli import cli
 from . import resolver
 
