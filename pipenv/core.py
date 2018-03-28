e2e2ac1ee10443ff7e4d9068063e9e77eb3ea413
frostming <mianghong@gmail.com>
2018-03-28 11:20:20 +0800

    Fix an error that was never hit

--- pipenv/core.py
+++ pipenv/core.py
@@ -290,7 +290,6 @@ def ensure_pipfile(validate=True, skip_requirements=False):
     if validate and project.virtualenv_exists and not PIPENV_SKIP_VALIDATION:
         # Ensure that Pipfile is using proper casing.
         p = project.parsed_pipfile
-        p.clear_pipfile_cache()
         changed = ensure_proper_casing(pfile=p)
         # Write changes out to disk.
         if changed:
@@ -299,6 +298,7 @@ def ensure_pipfile(validate=True, skip_requirements=False):
                 err=True,
             )
             project.write_toml(p)
+            project.clear_pipfile_cache()
 
 
 def find_python_from_py(python):
