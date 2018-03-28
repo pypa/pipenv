094044d50256da351fd1224b5d29efd5db40f583
Josh Friend <josh@fueledbycaffeine.com>
2018-03-28 18:25:59 -0400

    Python 3.6.5

--- pipenv/core.py
+++ pipenv/core.py
@@ -451,7 +451,7 @@ def ensure_python(three=None, python=None):
                     '3.3': '3.3.7',
                     '3.4': '3.4.8',
                     '3.5': '3.5.5',
-                    '3.6': '3.6.4',
+                    '3.6': '3.6.5',
                 }
                 try:
                     if len(python.split('.')) == 2:
