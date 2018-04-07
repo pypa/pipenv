e963674e6673c542983ab37847f8048d13b7fb18
Oliver Charlesworth <oliver.charlesworth@twosigmaiq.com>
2018-04-08 00:17:49 +0100

    Remove CI stuff from run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -11,42 +11,30 @@ if [[ ! -z "$TEST_SUITE" ]]; then
 	echo "Using TEST_SUITE=$TEST_SUITE"
 fi
 
-# If running in CI environment…
-if [[ ! -z "$CI" ]]; then
-	echo "Installing Pipenv…"
-	pip install -e "$(pwd)" --upgrade
-	pipenv install --deploy --system --dev
-
-    echo "Running tests…"
-	pipenv run time pytest -v -n auto tests -m "$TEST_SUITE" --tap-stream
-
-# Otherwise, we're on a development machine.
+# First, try MacOS…
+echo "Clearing Caches…"
+if [[ $(python -c "import sys; print(sys.platform)") == "darwin" ]]; then
+    CACHE_ROOT=~/Library/Caches
+# Otherwise, assume Linux…
 else
-	# First, try MacOS…
-	echo "Clearing Caches…"
-	if [[ $(python -c "import sys; print(sys.platform)") == "darwin" ]]; then
-	    CACHE_ROOT=~/Library/Caches
-	# Otherwise, assume Linux…
-	else
-	    CACHE_ROOT=~/.cache
-	fi
-	rm -fr ${CACHE_ROOT}/pip
-	rm -fr ${CACHE_ROOT}/pipenv
-
-	# If the lockfile hasn't changed, skip installs.
-	echo "Installing Pipenv…"
-	pip install -e "$(pwd)" --upgrade-strategy=only-if-needed
-
-	echo "Installing dependencies…"
-	PIPENV_PYTHON=2.7 pipenv run pip install -e . --upgrade
-	PIPENV_PYTHON=3.6 pipenv run pip install -e . --upgrade
-	PIPENV_PYTHON=2.7 pipenv install --dev
-	PIPENV_PYTHON=3.6 pipenv install --dev
-
-    echo "Running tests…"
-	PIPENV_PYTHON=2.7 pipenv run time pytest -v -n auto tests -m "$TEST_SUITE"
-	PIPENV_PYTHON=3.6 pipenv run time pytest -v -n auto tests -m "$TEST_SUITE"
-
-	# Cleanup junk.
-	rm -fr .venv
+    CACHE_ROOT=~/.cache
 fi
+rm -fr ${CACHE_ROOT}/pip
+rm -fr ${CACHE_ROOT}/pipenv
+
+# If the lockfile hasn't changed, skip installs.
+echo "Installing Pipenv…"
+pip install -e "$(pwd)" --upgrade-strategy=only-if-needed
+
+echo "Installing dependencies…"
+PIPENV_PYTHON=2.7 pipenv run pip install -e . --upgrade
+PIPENV_PYTHON=3.6 pipenv run pip install -e . --upgrade
+PIPENV_PYTHON=2.7 pipenv install --dev
+PIPENV_PYTHON=3.6 pipenv install --dev
+
+echo "Running tests…"
+PIPENV_PYTHON=2.7 pipenv run time pytest -v -n auto tests -m "$TEST_SUITE"
+PIPENV_PYTHON=3.6 pipenv run time pytest -v -n auto tests -m "$TEST_SUITE"
+
+# Cleanup junk.
+rm -fr .venv
