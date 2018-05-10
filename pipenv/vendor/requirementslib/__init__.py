56f3d6191e6065388bb2121b00c54de83642eb8c
Dan Ryan <dan@danryan.co>
2018-05-10 17:57:30 -0400

    Use vendored requirementslib for requirements
    - Also update patch for pip
    
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- pipenv/vendor/requirementslib/__init__.py
+++ pipenv/vendor/requirementslib/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding=utf-8 -*-
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 from .requirements import Requirement
--- pipenv/vendor/requirementslib/utils.py
+++ pipenv/vendor/requirementslib/utils.py
@@ -101,3 +101,32 @@ def pep423_name(name):
 
     else:
         return name
+
+
+def prepare_pip_source_args(sources, pip_args=None):
+    if pip_args is None:
+        pip_args = []
+    if sources:
+        # Add the source to pip9.
+        pip_args.extend(['-i', sources[0]['url']])
+        # Trust the host if it's not verified.
+        if not sources[0].get('verify_ssl', True):
+            pip_args.extend(
+                [
+                    '--trusted-host',
+                    urlparse(sources[0]['url']).netloc.split(':')[0],
+                ]
+            )
+        # Add additional sources as extra indexes.
+        if len(sources) > 1:
+            for source in sources[1:]:
+                pip_args.extend(['--extra-index-url', source['url']])
+                # Trust the host if it's not verified.
+                if not source.get('verify_ssl', True):
+                    pip_args.extend(
+                        [
+                            '--trusted-host',
+                            urlparse(source['url']).hostname,
+                        ]
+                    )
+    return pip_args
