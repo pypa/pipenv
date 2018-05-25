1e0882c74da5a3074bd5c8f2834f8a151966233f
Tzu-ping Chung <uranusjr@gmail.com>
2018-05-25 14:12:55 +0900

    Fix venv discovery
    If we don't have a project, we don't have an associated environment.

--- pipenv/project.py
+++ pipenv/project.py
@@ -216,8 +216,8 @@ class Project(object):
         return bool(self.requirements_location)
 
     def is_venv_in_project(self):
-        return (
-            PIPENV_VENV_IN_PROJECT or
+        return PIPENV_VENV_IN_PROJECT or (
+            self.project_directory and
             os.path.exists(os.path.join(self.project_directory, '.venv'))
         )
 
