3923be7fa2d395bc6573cb1b35ef73e7b111a20c
Mathieu Leduc-Hamel <mathieu.leduchamel@shopify.com>
2018-06-15 16:25:40 -0400

    Fixes PipenvInstance instanciation

--- tests/integration/test_index.py
+++ tests/integration/test_index.py
@@ -7,8 +7,8 @@ import pytest
 
 
 @pytest.mark.index
-def test_add_new_index(PipenvInstance):
-    with temp_environ(), PipenvInstance(chdir=True) as p:
+def test_add_new_index(PipenvInstance, pypi):
+    with temp_environ(), PipenvInstance(pypi=pypi) as p:
         mirror_url = "https://pypi.python.org/simple2"
 
         c = p.pipenv('index {0} pypi2'.format(mirror_url))
@@ -16,9 +16,9 @@ def test_add_new_index(PipenvInstance):
         assert c.return_code == 0
 
         assert p.pipfile['source'] == [{
-            'url': 'https://pypi.org/simple',
+            'url': "{0}/simple".format(pypi.url),
             'verify_ssl': True,
-            'name': 'pypi'
+            'name': 'custom'
         }, {
             'url': 'https://pypi.python.org/simple2',
             'verify_ssl': True,
@@ -27,17 +27,17 @@ def test_add_new_index(PipenvInstance):
 
 
 @pytest.mark.index
-def test_add_new_index_without_name(PipenvInstance):
-    with temp_environ(), PipenvInstance(chdir=True) as p:
+def test_add_new_index_without_name(PipenvInstance, pypi):
+    with temp_environ(), PipenvInstance(pypi=pypi) as p:
         mirror_url = "https://pypi.python.org/simple2"
 
         c = p.pipenv('index {0}'.format(mirror_url))
 
         assert c.return_code == 0
         assert p.pipfile['source'] == [{
-            'url': 'https://pypi.org/simple',
+            'url': "{0}/simple".format(pypi.url),
             'verify_ssl': True,
-            'name': 'pypi'
+            'name': 'custom'
         }, {
             'url': mirror_url,
             'verify_ssl': True,
@@ -45,40 +45,41 @@ def test_add_new_index_without_name(PipenvInstance):
 
 
 @pytest.mark.index
-def test_existing_index_name(PipenvInstance):
-    with temp_environ(), PipenvInstance(chdir=True) as p:
-        c = p.pipenv('index https://pypi.python.org/simple2 pypi')
+def test_existing_index_name(PipenvInstance, pypi):
+    with temp_environ(), PipenvInstance(pypi=pypi) as p:
+        c = p.pipenv('index https://pypi.python.org/simple2 custom')
 
         assert c.return_code == 0
 
         assert p.pipfile['source'] == [{
-            'url': 'https://pypi.org/simple',
+            'url': "{0}/simple".format(pypi.url),
             'verify_ssl': True,
-            'name': 'pypi'
+            'name': 'custom'
         }]
 
 
 @pytest.mark.index
-def test_existing_index_url(PipenvInstance):
-    with temp_environ(), PipenvInstance(chdir=True) as p:
-        c = p.pipenv('index https://pypi.org/simple pypi3')
+def test_existing_index_url(PipenvInstance, pypi):
+    with temp_environ(), PipenvInstance(pypi=pypi) as p:
+        mirror_url = "{0}/simple".format(pypi.url)
+        c = p.pipenv('index {0} pypi3'.format(mirror_url))
 
         assert c.return_code == 0
         assert p.pipfile['source'] == [{
-            'url': 'https://pypi.org/simple',
+            'url': mirror_url,
             'verify_ssl': True,
-            'name': 'pypi'
+            'name': 'custom'
         }]
 
 
 @pytest.mark.index
-def test_dry_run(PipenvInstance):
-    with temp_environ(), PipenvInstance(chdir=True) as p:
+def test_dry_run(PipenvInstance, pypi):
+    with temp_environ(), PipenvInstance(pypi=pypi) as p:
         c = p.pipenv('index https://pypi.org/simple2 pypi2 --dry-run')
 
         assert c.return_code == 0
         assert p.pipfile['source'] == [{
-            'url': 'https://pypi.org/simple',
+            'url': "{0}/simple".format(pypi.url),
             'verify_ssl': True,
-            'name': 'pypi'
+            'name': 'custom'
         }]
