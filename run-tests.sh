2addee4fd638ca397fe0ad17d37a2966df2173d5
Kenneth Reitz <me@kennethreitz.org>
2018-07-03 08:48:18 -0400

    3.7

--- run-tests.sh
+++ run-tests.sh
@@ -41,16 +41,16 @@ fi
 
 echo "Installing dependencies…"
 PIPENV_PYTHON=2.7 pipenv run pip install -e . --upgrade
-PIPENV_PYTHON=3.6 pipenv run pip install -e . --upgrade
+PIPENV_PYTHON=3.7 pipenv run pip install -e . --upgrade
 PIPENV_PYTHON=2.7 pipenv install --dev
-PIPENV_PYTHON=3.6 pipenv install --dev
+PIPENV_PYTHON=3.7 pipenv install --dev
 
 echo "$ pipenv run time pytest -v -n auto tests -m \"$TEST_SUITE\""
 # PIPENV_PYTHON=2.7 pipenv run time pytest -v -n auto tests -m "$TEST_SUITE" | prefix 2.7 &
 # PIPENV_PYTHON=3.6 pipenv run time pytest -v -n auto tests -m "$TEST_SUITE" | prefix 3.6
 # Better to run them sequentially.
-PIPENV_PYTHON=2.7 pipenv run time pytest -v -n auto tests -m "$TEST_SUITE"
-PIPENV_PYTHON=3.6 pipenv run time pytest -v -n auto tests -m "$TEST_SUITE"
+PIPENV_PYTHON=2.7 pipenv run time pytest
+PIPENV_PYTHON=3.7 pipenv run time pytest
 
 # test revendoring
 pip3 install --upgrade invoke requests parver
