c48d24c8cc4cefc8773bbff1cc91fbfb9c296f8c
Dan Ryan <dan@danryan.co>
2018-09-02 20:29:30 -0400

    Syntax error...
    Signed-off-by: Dan Ryan <dan@danryan.co>
    
    Update requirementslib
    
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- pipenv/vendor/requirementslib/models/vcs.py
+++ pipenv/vendor/requirementslib/models/vcs.py
@@ -24,12 +24,11 @@ class VCSRepository(object):
 
     def obtain(self):
         if not os.path.exists(self.checkout_directory):
-            self.repo_instance.obtain(self.checkout_directory)
+            self.repo_instance.unpack(self.checkout_directory)
         if self.ref:
-            self.checkout_ref(self.ref)
+            self.update(self.ref)
             self.commit_sha = self.get_commit_hash(self.ref)
         else:
-            self.ref = self.repo_instance.default_arg_rev
             if not self.commit_sha:
                 self.commit_sha = self.get_commit_hash()
 
