pypa/pipenv

commit 999761130e910bb211c75467d66be7db7547e7fe

Raise explicit exception for wrong which context

pipenv/core.py
@@ -106,7 +106,8 @@ def which(command, location=None, allow_global=False):
             project.virtualenv_location
             or os.environ.get("VIRTUAL_ENV", "")
         )
-    assert location and os.path.exists(location), "virtualenv not created"
+    if not location and os.path.exists(location):
+        raise RuntimeError("virtualenv not created nor specified")
     if not allow_global:
         if os.name == "nt":
             p = find_windows_executable(os.path.join(location, "Scripts"), command)

commit e2b8d544d2e0b4717a5564ab8c6226be034b79ed

fix virtualenv assertion bug
introduced in 999761130e910bb211c75467d66be7db7547e7fe.

pipenv/core.py
@@ -106,7 +106,7 @@ if PIPENV_NOSPIN:
 def which(command, location=None, allow_global=False):
     if not allow_global and location is None:
         location = project.virtualenv_location or os.environ.get("VIRTUAL_ENV", "")
-    if not location and os.path.exists(location):
+    if not (location and os.path.exists(location)):
         raise RuntimeError("virtualenv not created nor specified")
     if not allow_global:
         if os.name == "nt":