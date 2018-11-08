1555200463cd91babc1197e81bec15357d7f11f4
frostming <mianghong@gmail.com>
2018-11-08 14:59:59 +0800

    Add back some tests

--- tests/unit/test_vendor.py
+++ tests/unit/test_vendor.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 # We need to import the patched packages directly from sys.path, so the
 # identity checks can pass.
+import pipenv  # noqa
+
+import datetime
 import os
 
-import pipenv  # noqa
+import pytest
+import pytz
+import tomlkit
+
 from pipfile.api import PipfileParser
 
 
@@ -38,3 +44,44 @@ class TestPipfileParser:
         assert parsed_dict["list"][1] == {}
         assert parsed_dict["bool"] is True
         assert parsed_dict["none"] is None
+
+
+@pytest.mark.parametrize('dt, content', [
+    (   # Date.
+        datetime.date(1992, 8, 19),
+        '1992-08-19',
+    ),
+    (   # Naive time.
+        datetime.time(15, 10),
+        '15:10:00',
+    ),
+    (   # Aware time in UTC.
+        datetime.time(15, 10, tzinfo=pytz.UTC),
+        '15:10:00+00:00',
+    ),
+    (   # Aware local time.
+        datetime.time(15, 10, tzinfo=pytz.FixedOffset(8 * 60)),
+        '15:10:00+08:00',
+    ),
+    (   # Naive datetime.
+        datetime.datetime(1992, 8, 19, 15, 10),
+        '1992-08-19T15:10:00',
+    ),
+    (   # Aware datetime in UTC.
+        datetime.datetime(1992, 8, 19, 15, 10, tzinfo=pytz.UTC),
+        '1992-08-19T15:10:00Z',
+    ),
+    (   # Aware local datetime.
+        datetime.datetime(1992, 8, 19, 15, 10, tzinfo=pytz.FixedOffset(8 * 60)),
+        '1992-08-19T15:10:00+08:00',
+    ),
+])
+def test_token_date(dt, content):
+    item = tomlkit.item(dt)
+    assert item.as_string() == content
+
+
+def test_dump_nonascii_string():
+    content = 'name = "Stažené"\n'
+    toml_content = tomlkit.dumps(tomlkit.loads(content))
+    assert toml_content == content
