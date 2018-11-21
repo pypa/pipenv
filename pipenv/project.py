785943c6adb03f483738cf3613694800734ee703
Dan Ryan <dan@danryan.co>
2018-11-21 18:32:23 -0500

    Fix bug in lockfile generation
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- pipenv/project.py
+++ pipenv/project.py
@@ -785,10 +785,11 @@ class Project(object):
 
     def get_lockfile_meta(self):
         from .vendor.plette.lockfiles import PIPFILE_SPEC_CURRENT
-        sources = self.lockfile_content.get("_meta", {}).get("sources", [])
-        if not sources:
-            sources = self.pipfile_sources
-        elif not isinstance(sources, list):
+        if self.lockfile_exists:
+            sources = self.lockfile_content.get("_meta", {}).get("sources", [])
+        else:
+            sources = [dict(source) for source in self.parsed_pipfile["source"]]
+        if not isinstance(sources, list):
             sources = [sources,]
         return {
             "hash": {"sha256": self.calculate_pipfile_hash()},
