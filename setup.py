37c79915dfc6a86627133a47ef2491dca32edf39
Frost Ming <mianghong@gmail.com>
2019-01-04 21:44:11 +0800

    Remove typing dep

--- setup.py
+++ setup.py
@@ -29,8 +29,7 @@ required = [
     "virtualenv",
     'requests[security];python_version<"2.7"',
     'ordereddict;python_version<"2.7"',
-    'enum34; python_version<"3"',
-    'typing; python_version<"3.5"'
+    'enum34; python_version<"3"'
 ]
 
 
