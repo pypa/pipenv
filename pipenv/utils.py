75f62a7ad6dbc00d2dfea97fc9306863b7599aa5
Lee Bontecou <lee.bontecou@inquest.net>
2019-02-22 16:42:10 -0600

    fix bug where package_url was being used instead of just url

--- pipenv/utils.py
+++ pipenv/utils.py
@@ -224,7 +224,7 @@ def prepare_pip_source_args(sources, pip_args=None):
                 pip_args.extend(["--extra-index-url", url])
                 # Trust the host if it's not verified.
                 if not source.get("verify_ssl", True):
-                    url_parts = urllib3_util.parse_url(package_url)
+                    url_parts = urllib3_util.parse_url(url)
                     url_port = ":{0}".format(url_parts.port) if url_parts.port else ""
                     pip_args.extend(
                         ["--trusted-host", "{0}{1}".format(url_parts.host, url_port)]
