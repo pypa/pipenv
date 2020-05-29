dffcac123352249a7b5e40158a94663e9f1c1342
frostming <frostming@tencent.com>
2020-05-29 14:18:13 +0800

    Only ignore errors for CI builds

--- pipenv/core.py
+++ pipenv/core.py
@@ -126,6 +126,12 @@ def do_clear():
 
     try:
         vistir.path.rmtree(PIPENV_CACHE_DIR, onerror=vistir.path.handle_remove_readonly)
+        # Other processes may be writing into this directory simultaneously.
+        vistir.path.rmtree(
+            locations.USER_CACHE_DIR,
+            ignore_errors=environments.PIPENV_IS_CI,
+            onerror=vistir.path.handle_remove_readonly
+        )
     except OSError as e:
         # Ignore FileNotFoundError. This is needed for Python 2.7.
         import errno
@@ -133,8 +139,6 @@ def do_clear():
         if e.errno == errno.ENOENT:
             pass
         raise
-    # Other processes may be writing into this directory simultaneously.
-    vistir.path.rmtree(locations.USER_CACHE_DIR, ignore_errors=True)
 
 
 def load_dot_env():
