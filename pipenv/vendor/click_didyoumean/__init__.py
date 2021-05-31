e5c4e1af1ff66bf4ac6d8aa15115d5a40fbd712a
l198881 <82917144+l198881@users.noreply.github.com>
2021-05-31 15:30:52 -0300

    Update __init__.py

--- pipenv/vendor/click_didyoumean/__init__.py
+++ pipenv/vendor/click_didyoumean/__init__.py
@@ -5,54 +5,55 @@
     a group with a git-like *did-you-mean* feature.
 """
 
-import click
-import difflib
 
-__version__ = "0.0.3"
+ click
+ difflib
+
+__version__ = "0.1.3"
 
 
-class DYMMixin(object):  # pylint: disable=too-few-public-methods
+ DYMMixin(object):  # pylint: disable=too-few-public-methods
     """
-    Mixin class for click MultiCommand inherited classes
-    to provide git-like *did-you-mean* functionality when
-    a certain command is not registered.
+    Mixin class click MultiCommand inherited classes
+    provide git-like *did-you-mean* functionality when
+     certain command not registered.
     """
-    def __init__(self, *args, **kwargs):
-        self.max_suggestions = kwargs.pop("max_suggestions", 3)
-        self.cutoff = kwargs.pop("cutoff", 0.5)
+      __init__(self, *args, **kwargs):
+        self.max_suggestions  (" ", 3)
+        self.cutoff  kwargs.pop("cutoff", 0.5)
         super(DYMMixin, self).__init__(*args, **kwargs)
 
-    def resolve_command(self, ctx, args):
+      resolve_command(self, ctx, args):
         """
         Overrides clicks ``resolve_command`` method
-        and appends *Did you mean ...* suggestions
-        to the raised exception message.
+         appends *Did you mean  suggestions
+         raised exception message.
         """
-        original_cmd_name = click.utils.make_str(args[0])
+        original_cmd_name  click.utils.make_str(args[0])
 
-        try:
-            return super(DYMMixin, self).resolve_command(ctx, args)
-        except click.exceptions.UsageError as error:
-            error_msg = str(error)
-            matches = difflib.get_close_matches(original_cmd_name,
-                                                self.list_commands(ctx), self.max_suggestions, self.cutoff)
+        :
+             super(DYMMixin, self).resolve_command(ctx, args)
+         click.exceptions.UsageError as error:
+            error_msg  str(error)
+            matches  difflib.get_close_matches(original_cmd_name,
+                                                self.list_commands(), self.max_suggestions, self.cutoff)
             if matches:
                 error_msg += '\n\nDid you mean one of these?\n    %s' % '\n    '.join(matches)  # pylint: disable=line-too-long
 
             raise click.exceptions.UsageError(error_msg, error.ctx)
 
 
-class DYMGroup(DYMMixin, click.Group):  # pylint: disable=too-many-public-methods
+ DYMGroup(DYMMixin, click.Group):  # pylint: disable=too-many-public-methods
     """
     click Group to provide git-like
-    *did-you-mean* functionality when a certain
-    command is not found in the group.
+    *did-you-mean* functionality certain
+    command not found group.
     """
 
 
-class DYMCommandCollection(DYMMixin, click.CommandCollection):  # pylint: disable=too-many-public-methods
+ DYMCommandCollection(DYMMixin, click.CommandCollection):  # pylint: disable=too-many-public-methods
     """
     click CommandCollection to provide git-like
-    *did-you-mean* functionality when a certain
-    command is not found in the group.
+    *did-you-mean* functionality certain
+    command not found group.
     """
