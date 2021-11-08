0f67a112dc8eed683cb88bd221c22c0deb2fbe30
Frost Ming <mianghong@gmail.com>
2021-11-08 14:58:18 +0800

    Populate the PATH env in pipenv run

--- tests/integration/test_install_uri.py
+++ tests/integration/test_install_uri.py
@@ -257,7 +257,6 @@ def test_get_vcs_refs(PipenvInstance_NoPyPI):
 @pytest.mark.urls
 @pytest.mark.install
 @pytest.mark.needs_internet
-@pytest.mark.py3_only
 def test_vcs_entry_supersedes_non_vcs(PipenvInstance):
     """See issue #2181 -- non-editable VCS dep was specified, but not showing up
     in the lockfile -- due to not running pip install before locking and not locking
