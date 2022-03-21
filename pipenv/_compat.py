984abe1a60e50576dbb779543144db98230d1b65
Matt Davis <matteius@gmail.com>
2022-03-21 04:56:25 -0400

    remove deadcode from _compat.py

--- pipenv/_compat.py
+++ pipenv/_compat.py
@@ -14,8 +14,6 @@ warnings.filterwarnings("ignore", category=ResourceWarning)
 __all__ = [
     "getpreferredencoding",
     "DEFAULT_ENCODING",
-    "canonical_encoding_name",
-    "force_encoding",
     "UNICODE_TO_ASCII_TRANSLATION_MAP",
     "decode_output",
     "fix_utf8",
@@ -31,70 +29,6 @@ def getpreferredencoding():
 
 
 DEFAULT_ENCODING = getpreferredencoding()
-
-
-def canonical_encoding_name(name):
-    import codecs
-
-    try:
-        codec = codecs.lookup(name)
-    except LookupError:
-        return name
-    else:
-        return codec.name
-
-
-# From https://github.com/CarlFK/veyepar/blob/5c5de47/dj/scripts/fixunicode.py
-# MIT LIcensed, thanks Carl!
-def force_encoding():
-    try:
-        stdout_isatty = sys.stdout.isatty
-        stderr_isatty = sys.stderr.isatty
-    except AttributeError:
-        return DEFAULT_ENCODING, DEFAULT_ENCODING
-    else:
-        if not (stdout_isatty() and stderr_isatty()):
-            return DEFAULT_ENCODING, DEFAULT_ENCODING
-    stdout_encoding = canonical_encoding_name(sys.stdout.encoding)
-    stderr_encoding = canonical_encoding_name(sys.stderr.encoding)
-    if sys.platform == "win32":
-        return DEFAULT_ENCODING, DEFAULT_ENCODING
-    if stdout_encoding != "utf-8" or stderr_encoding != "utf-8":
-
-        try:
-            from ctypes import c_char_p, py_object, pythonapi
-        except ImportError:
-            return DEFAULT_ENCODING, DEFAULT_ENCODING
-        try:
-            PyFile_SetEncoding = pythonapi.PyFile_SetEncoding
-        except AttributeError:
-            return DEFAULT_ENCODING, DEFAULT_ENCODING
-        else:
-            PyFile_SetEncoding.argtypes = (py_object, c_char_p)
-            if stdout_encoding != "utf-8":
-                try:
-                    was_set = PyFile_SetEncoding(sys.stdout, "utf-8")
-                except OSError:
-                    was_set = False
-                if not was_set:
-                    stdout_encoding = DEFAULT_ENCODING
-                else:
-                    stdout_encoding = "utf-8"
-
-            if stderr_encoding != "utf-8":
-                try:
-                    was_set = PyFile_SetEncoding(sys.stderr, "utf-8")
-                except OSError:
-                    was_set = False
-                if not was_set:
-                    stderr_encoding = DEFAULT_ENCODING
-                else:
-                    stderr_encoding = "utf-8"
-
-    return stdout_encoding, stderr_encoding
-
-
-OUT_ENCODING, ERR_ENCODING = force_encoding()
 
 
 UNICODE_TO_ASCII_TRANSLATION_MAP = {
