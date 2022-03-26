b827bb20f81ab5c6137c5eff1de930c448b4016d
Matt Davis <matteius@gmail.com>
2022-03-25 23:45:26 -0400

    refactor this test into a version that also check quiet mode, move to correct test file.

--- tests/integration/test_run.py
+++ tests/integration/test_run.py
@@ -82,3 +82,27 @@ def test_run_with_usr_env_shebang(PipenvInstance):
         project = Project()
         lines = [line.strip() for line in c.stdout.splitlines()]
         assert all(line == project.virtualenv_location for line in lines)
+
+
+@pytest.mark.run
+@pytest.mark.parametrize('quiet', [True, False])
+def test_scripts_resolve_dot_env_vars(quiet, PipenvInstance):
+    with PipenvInstance() as p:
+        with open(".env", "w") as f:
+            contents = """
+HELLO=WORLD
+            """.strip()
+            f.write(contents)
+
+        with open(p.pipfile_path, "w") as f:
+            contents = """
+[scripts]
+hello = "echo $HELLO"
+            """.strip()
+            f.write(contents)
+        if quiet:
+            c = p.pipenv('run --quiet hello')
+        else:
+            c = p.pipenv('run hello')
+        assert c.returncode == 0
+        assert 'WORLD\n' == c.stdout
