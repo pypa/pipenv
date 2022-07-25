6f7d11b45b957346b417acbf94805e64a9aae5be
Oz N Tiram <oz.tiram@gmail.com>
2022-07-25 17:18:43 +0200

    Update tomlkit==0.9.2
    Used:
    
     python -m invoke vendoring.update --package=tomlkit

--- pipenv/vendor/tomlkit/toml_char.py
+++ pipenv/vendor/tomlkit/toml_char.py
@@ -1,18 +1,11 @@
 import string
 
-from ._compat import PY2
-from ._compat import unicode
+from functools import lru_cache
 
 
-if PY2:
-    from pipenv.vendor.backports.functools_lru_cache import lru_cache
-else:
-    from functools import lru_cache
-
-
-class TOMLChar(unicode):
+class TOMLChar(str):
     def __init__(self, c):
-        super(TOMLChar, self).__init__()
+        super().__init__()
 
         if len(self) > 1:
             raise ValueError("A TOML character must be of length 1")
@@ -25,42 +18,42 @@ class TOMLChar(unicode):
     WS = SPACES + NL
 
     @lru_cache(maxsize=None)
-    def is_bare_key_char(self):  # type: () -> bool
+    def is_bare_key_char(self) -> bool:
         """
         Whether the character is a valid bare key name or not.
         """
         return self in self.BARE
 
     @lru_cache(maxsize=None)
-    def is_kv_sep(self):  # type: () -> bool
+    def is_kv_sep(self) -> bool:
         """
         Whether the character is a valid key/value separator ot not.
         """
         return self in self.KV
 
     @lru_cache(maxsize=None)
-    def is_int_float_char(self):  # type: () -> bool
+    def is_int_float_char(self) -> bool:
         """
         Whether the character if a valid integer or float value character or not.
         """
         return self in self.NUMBER
 
     @lru_cache(maxsize=None)
-    def is_ws(self):  # type: () -> bool
+    def is_ws(self) -> bool:
         """
         Whether the character is a whitespace character or not.
         """
         return self in self.WS
 
     @lru_cache(maxsize=None)
-    def is_nl(self):  # type: () -> bool
+    def is_nl(self) -> bool:
         """
         Whether the character is a new line character or not.
         """
         return self in self.NL
 
     @lru_cache(maxsize=None)
-    def is_spaces(self):  # type: () -> bool
+    def is_spaces(self) -> bool:
         """
         Whether the character is a space or not
         """
--- pipenv/vendor/tomlkit/toml_file.py
+++ pipenv/vendor/tomlkit/toml_file.py
@@ -1,24 +1,23 @@
-import io
-
-from typing import Any
-from typing import Dict
-
 from .api import loads
 from .toml_document import TOMLDocument
 
 
-class TOMLFile(object):
+class TOMLFile:
     """
     Represents a TOML file.
+
+    :param path: path to the TOML file
     """
 
-    def __init__(self, path):  # type: (str) -> None
+    def __init__(self, path: str) -> None:
         self._path = path
 
-    def read(self):  # type: () -> TOMLDocument
-        with io.open(self._path, encoding="utf-8") as f:
+    def read(self) -> TOMLDocument:
+        """Read the file content as a :class:`tomlkit.toml_document.TOMLDocument`."""
+        with open(self._path, encoding="utf-8", newline="") as f:
             return loads(f.read())
 
-    def write(self, data):  # type: (TOMLDocument) -> None
-        with io.open(self._path, "w", encoding="utf-8") as f:
+    def write(self, data: TOMLDocument) -> None:
+        """Write the TOMLDocument to the file."""
+        with open(self._path, "w", encoding="utf-8", newline="") as f:
             f.write(data.as_string())
