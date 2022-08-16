1c7dab5bcc1dc0166570766887370d6f35626793
Matt Davis <matteius@gmail.com>
2022-08-16 17:54:35 -0400

    Revise how we do the import patch to include only module pipenv.

--- pipenv/patched/pip/__main__.py
+++ pipenv/patched/pip/__main__.py
@@ -26,7 +26,13 @@ if __name__ == "__main__":
     warnings.filterwarnings(
         "ignore", category=DeprecationWarning, module=".*packaging\\.version"
     )
-    sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
+    import importlib.util
+    import sys
+    spec = importlib.util.spec_from_file_location(
+        "pipenv", location=os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "__init__.py"))
+    pipenv = importlib.util.module_from_spec(spec)
+    sys.modules["pipenv"] = pipenv
+    spec.loader.exec_module(pipenv)
     from pipenv.patched.pip._internal.cli.main import main as _main
 
     sys.exit(_main())
