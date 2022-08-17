2b4ef249112db794e13639f1ebfc23e86d78adf3
Matt Davis <matteius@gmail.com>
2022-08-17 02:32:38 -0400

    Revert "Revert "Remove yaspin and termcolor""
    This reverts commit 59bebd6dc9ad81d12f8890326cd65d11376d5428.

--- pipenv/vendor/yaspin/__init__.py
+++
@@ -1,7 +0,0 @@
-# :copyright: (c) 2021 by Pavlo Dmytrenko.
-# :license: MIT, see LICENSE for more details.
-from .api import kbi_safe_yaspin, yaspin
-from .base_spinner import Spinner
-
-
-__all__ = ("yaspin", "kbi_safe_yaspin", "Spinner")
