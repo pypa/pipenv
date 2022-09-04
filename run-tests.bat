b3ecbb0c4a62ad365c00d8250f15c42545c4cc68
Matt Davis <matteius@gmail.com>
2022-09-04 00:23:29 -0400

    Try to condtionally invoke different start pypi-server commands

--- run-tests.bat
+++ run-tests.bat
@@ -1,15 +1,6 @@
-rem If you want to use a ramdisk, use this section:
 
-rem imdisk -a -s 4G -m R: -p "FS:NTFS /y"
-rem if you are using a ram disk, you should comment the following substitution line out
-subst R: %TEMP%
-
-set TMP=R:\\
-set TEMP=R:\\
-set WORKON_HOME=R:\\
-set RAM_DISK=R:\\
-
-R:\.venv\Scripts\pip install -e .[test] --upgrade --upgrade-strategy=only-if-needed
-R:\.venv\Scripts\pipenv install --dev
+pip install -e .[test] --upgrade --upgrade-strategy=only-if-needed
+pipenv install --dev
 git submodule sync && git submodule update --init --recursive
-R:\.venv\Scripts\pipenv run pytest -n auto -v tests
+cmd /c start pipenv run pypi-server run -v --host=0.0.0.0 --port=8080 --hash-algo=sha256 --disable-fallback ./tests/pypi/ ./tests/fixtures
+pipenv run pytest -n auto -v tests
