79aeff5eba479d17e5d8742583a637ab10e45618
Oz N Tiram <oz.tiram@gmail.com>
2022-09-12 23:14:05 +0200

    Replace pipfile.Pipefile with plette.Pipefile
    Plette has a better code base and the same API.

--- pipenv/core.py
+++ pipenv/core.py
@@ -14,7 +14,6 @@ from typing import Dict, List, Optional, Union
 
 from pipenv import environments, exceptions, pep508checker
 from pipenv._compat import decode_for_output, fix_utf8
-from pipenv.patched import pipfile
 from pipenv.patched.pip._internal.build_env import _get_runnable_pip
 from pipenv.patched.pip._internal.exceptions import PipError
 from pipenv.patched.pip._internal.network.session import PipSession
@@ -49,7 +48,7 @@ from pipenv.utils.shell import (
     system_which,
 )
 from pipenv.utils.spinner import create_spinner
-from pipenv.vendor import click, dotenv, vistir
+from pipenv.vendor import click, dotenv, plette, vistir
 from pipenv.vendor.requirementslib.models.requirements import Requirement
 
 if MYPY_RUNNING:
@@ -2832,7 +2831,7 @@ def do_check(
             )
             sys.exit(1)
     # Load the pipfile.
-    p = pipfile.Pipfile.load(project.pipfile_location)
+    p = plette.Pipfile.load(project.pipfile_location)
     failed = False
     # Assert each specified requirement.
     for marker, specifier in p.data["_meta"]["requires"].items():
