5db5472fd1a7ebc18853b35564fe91d51be7b507
Matt Davis <matteius@gmail.com>
2022-11-22 18:23:07 -0500

    Support python 3.11

--- setup.py
+++ setup.py
@@ -82,6 +82,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
     ],
