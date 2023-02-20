d89d165f5e0082882ac0ec81db97e31932205a6f
Matt Davis <matteius@gmail.com>
2023-02-20 03:52:06 -0500

    improve test

--- tests/integration/test_install_twists.py
+++ tests/integration/test_install_twists.py
@@ -118,6 +118,8 @@ Requests = "==2.14.0"   # Inline comment
         c = p.pipenv("install requests==2.18.4")
         assert c.returncode == 0
         assert "Requests" not in p.pipfile["packages"]
+        assert "requests" in p.pipfile["packages"]
+        assert p.pipfile["packages"]["requests"] == "==2.18.4"
         c = p.pipenv("install python_DateUtil")
         assert c.returncode == 0
         assert "python-dateutil" in p.pipfile["packages"]
