e32a5e4a0c8123b9a698257b6eabbe85e3100c2f
Matt Davis <matteius@gmail.com>
2023-04-29 17:07:06 -0400

    remove reference to WindowsFinder

--- pipenv/vendor/pythonfinder/__init__.py
+++ pipenv/vendor/pythonfinder/__init__.py
@@ -1,19 +1,8 @@
-from __future__ import absolute_import, print_function
-
-# Add NullHandler to "pythonfinder" logger, because Python2's default root
-# logger has no handler and warnings like this would be reported:
-#
-# > No handlers could be found for logger "pythonfinder.models.pyenv"
-import logging
-
 from .exceptions import InvalidPythonVersion
-from .models import SystemPath, WindowsFinder
+from .models import SystemPath
 from .pythonfinder import Finder
 
 __version__ = "1.3.2"
 
-
-logger = logging.getLogger(__name__)
-logger.addHandler(logging.NullHandler())
 
 __all__ = ["Finder", "SystemPath", "InvalidPythonVersion"]
