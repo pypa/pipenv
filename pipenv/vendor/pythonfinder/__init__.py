d5dd2df627d814f3e20d6331386dc610b553031f
Matt Davis <matteius@gmail.com>
2023-05-19 18:17:52 -0400

    Bump pythonfinder to 2.0.3

--- pipenv/vendor/pythonfinder/__init__.py
+++ pipenv/vendor/pythonfinder/__init__.py
@@ -4,7 +4,7 @@ from .exceptions import InvalidPythonVersion
 from .models import SystemPath
 from .pythonfinder import Finder
 
-__version__ = "2.0.2"
+__version__ = "2.0.3"
 
 
 __all__ = ["Finder", "SystemPath", "InvalidPythonVersion"]
