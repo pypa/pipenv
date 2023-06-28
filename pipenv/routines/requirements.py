01ddf00090c2a99067b3922f70359e78ed09cd9d
Matt Davis <matteius@gmail.com>
2023-06-28 09:50:43 -0400

    Apply suggestions from code review
    Co-authored-by: Michael Marino <mmarino@gmail.com>

--- pipenv/routines/requirements.py
+++ pipenv/routines/requirements.py
@@ -11,14 +11,14 @@ def requirements_from_deps(deps, include_hashes=True, include_markers=True):
     for package_name, package_info in deps.items():
         # Handling git repositories
         if "git" in package_info:
-            git = package_info["git"].replace("ssh://", "https://")
+            git = package_info["git"]
             ref = package_info.get("ref", "")
             extras = (
                 "[{}]".format(",".join(package_info.get("extras", [])))
                 if "extras" in package_info
                 else ""
             )
-            pip_package = f'"{package_name}{extras} @ git+{git}@{ref}"'
+            pip_package = f"{package_name}{extras} @ git+{git}@{ref}"
         else:
             # Handling packages with hashes and markers
             version = package_info.get("version", "").replace("==", "")
