f661f497942941aff106e49baa2d810023daf5ac
Oz N Tiram <oz.tiram@gmail.com>
2023-07-21 12:17:20 +0200

    Remove secho from pipenv.environment

--- pipenv/utils/__init__.py
+++ pipenv/utils/__init__.py
@@ -1,3 +1,8 @@
 import logging
 
+from pipenv.patched.pip._vendor import rich
+`
 logging.basicConfig(level=logging.ERROR)
+
+console = rich.console.Console()
+err = rich.console.Console(stderr=True)
