757fc9a702bb81f210fa52aca4f38e8f44261bd7
Fabian Meeßen <fabian.meessen1997@gmail.com>
2023-09-30 21:44:34 +0200

    set regular logging level to ERROR

--- pipenv/utils/__init__.py
+++ pipenv/utils/__init__.py
@@ -2,7 +2,7 @@ import logging
 
 from pipenv.patched.pip._vendor.rich.console import Console
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.ERROR)
 
 console = Console()
 err = Console(stderr=True)
