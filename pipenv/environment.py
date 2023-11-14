55258f71c78026a0bd02c0bf389f0b4d7d9728f1
Matt Davis <matteius@gmail.com>
2023-11-14 18:01:15 -0500

    Assume that vcs and direct URL installs need to be reinstalled (#5936)
    * Assume the vcs and direct URL installs need to be reinstalled

--- pipenv/environment.py
+++ pipenv/environment.py
@@ -22,8 +22,6 @@ from pipenv.patched.pip._vendor import pkg_resources
 from pipenv.patched.pip._vendor.packaging.specifiers import SpecifierSet
 from pipenv.patched.pip._vendor.packaging.utils import canonicalize_name
 from pipenv.utils import console
-from pipenv.utils.constants import VCS_LIST
-from pipenv.utils.dependencies import as_pipfile
 from pipenv.utils.fileutils import normalize_path, temp_path
 from pipenv.utils.funktools import chunked, unnest
 from pipenv.utils.indexes import prepare_pip_source_args
@@ -776,24 +774,10 @@ class Environment:
                     parsed_url = urlparse(requested_path)
                     local_path = parsed_url.path
                 return requested_path and os.path.samefile(local_path, match.location)
-            elif match.has_metadata("direct_url.json"):
-                direct_url_metadata = json.loads(match.get_metadata("direct_url.json"))
-                requested_revision = direct_url_metadata.get("vcs_info", {}).get(
-                    "requested_revision", ""
-                )
-                vcs_type = direct_url_metadata.get("vcs_info", {}).get("vcs", "")
-                _, pipfile_part = as_pipfile(req).popitem()
-                vcs_ref = ""
-                for vcs in VCS_LIST:
-                    if pipfile_part.get(vcs):
-                        vcs_ref = pipfile_part[vcs].rsplit("@", 1)[-1]
-                        break
-                return (
-                    vcs_type == req.link.scheme
-                    and vcs_ref == requested_revision
-                    and direct_url_metadata["url"] == pipfile_part[req.link.scheme]
-                )
-            elif req.link.is_vcs:
+            elif match.has_metadata("direct_url.json") or (req.link and req.link.is_vcs):
+                # Direct URL installs and VCS installs we assume are not satisfied
+                # since due to skip-lock we may be installing from Pipfile we have insufficient
+                # information to determine if a branch or ref has actually changed.
                 return False
             return True
         return False
