5a929cee7104bd9ff4caa4025af9a3b796f72747
Matt Davis <matteius@gmail.com>
2023-12-01 07:13:38 -0500

    Initial take at making uninstall like the inverse of upgrade.

--- pipenv/routines/uninstall.py
+++ pipenv/routines/uninstall.py
@@ -7,14 +7,12 @@ from pipenv.patched.pip._vendor.packaging.utils import canonicalize_name
 from pipenv.routines.lock import do_lock
 from pipenv.utils.dependencies import (
     expansive_install_req_from_line,
-    get_canonical_names,
-    get_lockfile_section_using_pipfile_category,
     get_pipfile_category_using_lockfile_section,
     pep423_name,
 )
 from pipenv.utils.processes import run_command, subprocess_run
-from pipenv.utils.project import ensure_project
 from pipenv.utils.requirements import BAD_PACKAGES
+from pipenv.utils.resolver import venv_resolve_deps
 from pipenv.utils.shell import cmd_list_to_shell, project_python
 from pipenv.vendor import click
 
@@ -32,143 +30,87 @@ def do_uninstall(
     ctx=None,
     categories=None,
 ):
-    # Automatically use an activated virtualenv.
-    if project.s.PIPENV_USE_SYSTEM:
-        system = True
-    # Ensure that virtualenv is available.
-    ensure_project(project, python=python, pypi_mirror=pypi_mirror)
-    # Uninstall all dependencies, if --all was provided.
+    # Initialization similar to the upgrade function
     if not any([packages, editable_packages, all_dev, all]):
         raise exceptions.PipenvUsageError("No package provided!", ctx=ctx)
+
     if not categories:
-        categories = project.get_package_categories(for_lockfile=True)
-    editable_pkgs = []
-    for p in editable_packages:
-        if p:
-            install_req, name = expansive_install_req_from_line(f"-e {p}")
-            editable_pkgs.append(name)
-    packages += editable_pkgs
-    package_names = {p for p in packages if p}
-    package_map = {canonicalize_name(p): p for p in packages if p}
-    installed_package_names = project.installed_package_names
-    if project.lockfile_exists:
-        project_pkg_names = project.lockfile_package_names
-    else:
-        project_pkg_names = project.pipfile_package_names
-    # Uninstall [dev-packages], if --dev was provided.
-    if all_dev:
-        if (
-            "dev-packages" not in project.parsed_pipfile
-            and not project_pkg_names["develop"]
-        ):
-            click.echo(
-                click.style(
-                    "No {} to uninstall.".format(
-                        click.style("[dev-packages]", fg="yellow")
-                    ),
-                    bold=True,
-                )
-            )
-            return
-        click.secho(
-            click.style(
-                "Un-installing {}...".format(click.style("[dev-packages]", fg="yellow")),
-                bold=True,
-            )
-        )
-        preserve_packages = set()
-        dev_packages = set()
-        for category in project.get_package_categories(for_lockfile=True):
-            if category == "develop":
-                dev_packages |= set(project_pkg_names[category])
-            else:
-                preserve_packages |= set(project_pkg_names[category])
+        categories = ["default"]
 
-        package_names = dev_packages - preserve_packages
+    package_args = list(packages) + [f"-e {pkg}" for pkg in editable_packages]
 
-    # Remove known "bad packages" from the list.
-    bad_pkgs = get_canonical_names(BAD_PACKAGES)
-    ignored_packages = bad_pkgs & set(package_map)
-    for ignored_pkg in get_canonical_names(ignored_packages):
-        if project.s.is_verbose():
-            click.echo(f"Ignoring {ignored_pkg}.", err=True)
-        package_names.discard(package_map[ignored_pkg])
-
-    used_packages = project_pkg_names["combined"] & installed_package_names
-    failure = False
-    if all:
-        click.echo(
-            click.style(
-                "Un-installing all {} and {}...".format(
-                    click.style("[dev-packages]", fg="yellow"),
-                    click.style("[packages]", fg="yellow"),
-                ),
-                bold=True,
-            )
-        )
-        do_purge(project, bare=False, allow_global=system)
-        sys.exit(0)
-
-    selected_pkg_map = {canonicalize_name(p): p for p in package_names}
-    packages_to_remove = [
-        package_name
-        for normalized, package_name in selected_pkg_map.items()
-        if normalized in (used_packages - bad_pkgs)
-    ]
-    lockfile = project.get_or_create_lockfile(categories=categories)
+    # Determine packages and their dependencies for removal
     for category in categories:
-        category = get_lockfile_section_using_pipfile_category(category)
-        for normalized_name, package_name in selected_pkg_map.items():
-            if normalized_name in project.lockfile_content[category]:
-                click.echo(
-                    "{} {} {} {}".format(
-                        click.style("Removing", fg="cyan"),
-                        click.style(package_name, fg="green"),
-                        click.style("from", fg="cyan"),
-                        click.style("Pipfile.lock...", fg="white"),
-                    )
-                )
-                if normalized_name in lockfile[category]:
-                    del lockfile[category][normalized_name]
-                lockfile.write()
+        pipfile_category = get_pipfile_category_using_lockfile_section(category)
 
-            pipfile_category = get_pipfile_category_using_lockfile_section(category)
-            if project.remove_package_from_pipfile(
-                package_name, category=pipfile_category
-            ):
-                click.secho(
-                    f"Removed {package_name} from Pipfile category {pipfile_category}",
-                    fg="green",
-                )
+        for package in package_args[:]:
+            install_req, _ = expansive_install_req_from_line(package, expand_env=True)
 
-    for normalized_name, package_name in selected_pkg_map.items():
-        still_remains = False
-        for category in project.get_package_categories():
-            if project.get_package_name_in_pipfile(normalized_name, category=category):
-                still_remains = True
-        if not still_remains:
-            # Uninstall the package.
-            if package_name in packages_to_remove:
-                click.secho(
-                    f"Uninstalling {click.style(package_name)}...",
-                    fg="green",
-                    bold=True,
-                )
-                with project.environment.activated():
-                    cmd = [
-                        project_python(project, system=system),
-                        get_runnable_pip(),
-                        "uninstall",
-                        package_name,
-                        "-y",
-                    ]
-                    c = run_command(cmd, is_verbose=project.s.is_verbose())
-                    click.secho(c.stdout, fg="cyan")
-                    if c.returncode != 0:
-                        failure = True
+            name, normalized_name, pipfile_entry = project.generate_package_pipfile_entry(
+                install_req, package, category=pipfile_category
+            )
 
+            if normalized_name not in project.lockfile_content[category]:
+                continue
+
+            # Create a lock data for current state minus the package being removed
+            current_lock_data = project.lockfile_content[category].copy()
+            del current_lock_data[normalized_name]
+
+            # Resolve dependencies with the package removed
+            resolved_lock_data = venv_resolve_deps(
+                current_lock_data,
+                which=project._which,
+                project=project,
+                lockfile={},
+                category=pipfile_category,
+                pre=False,  # Assuming pre-releases are not handled here
+                allow_global=system,
+                pypi_mirror=pypi_mirror,
+            )
+
+            # Determine which dependencies are no longer needed
+            deps_to_remove = [
+                dep for dep in current_lock_data if dep not in resolved_lock_data
+            ]
+
+            # Remove unnecessary dependencies from Pipfile and lockfile
+            for dep in deps_to_remove:
+                if project.remove_package_from_pipfile(dep, category=pipfile_category):
+                    del project.lockfile_content[category][dep]
+
+    # Perform uninstallation of packages and dependencies
+    failure = False
+    for package in package_args:
+        normalized_name = canonicalize_name(package)
+
+        # Check if the package is actually installed
+        if normalized_name not in project.installed_package_names:
+            click.echo(f"Package {package} not found and cannot be uninstalled.")
+            continue
+
+        # Execute the uninstall command for the package
+        click.secho(f"Uninstalling {package}...", fg="green", bold=True)
+        with project.environment.activated():
+            cmd = [
+                project_python(project, system=system),
+                get_runnable_pip(),
+                "uninstall",
+                package,
+                "-y",
+            ]
+            c = run_command(cmd, is_verbose=project.s.is_verbose())
+            click.secho(c.stdout, fg="cyan")
+            if c.returncode != 0:
+                click.echo(f"Error occurred while uninstalling package {package}.")
+                failure = True
+                continue
+
+    # Finalize changes
+    project.write_lockfile(project.lockfile_content)
     if lock:
         do_lock(project, system=system, pypi_mirror=pypi_mirror)
+
     sys.exit(int(failure))
 
 
