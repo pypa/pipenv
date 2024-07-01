4c15cb0887cdbb21c0833f3345fe13d07da1caf7
Aiden Cullo <aidencullo@gmail.com>
2024-07-01 12:59:08 -0400

    fix pipenv call without options

--- pipenv/utils/__init__.py
+++ pipenv/utils/__init__.py
@@ -3,6 +3,5 @@ import logging
 from pipenv.patched.pip._vendor.rich.console import Console
 
 logging.basicConfig(level=logging.INFO)
-
-console = Console()
+console = Console(highlight=False)
 err = Console(stderr=True)
