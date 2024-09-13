94a0ed828115e634a8a3ce595652a19ba1b62431
Matt Davis <matteius@gmail.com>
2024-09-13 12:58:14 -0400

    fix test

--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -129,12 +129,8 @@ def test_pipenv_graph_reverse(pipenv_instance_private_pypi):
                 openpyxl_dep = re.search(r'^openpyxl',
                                          output,
                                          flags=re.MULTILINE | re.IGNORECASE)
-                assert openpyxl_dep is None, f'openpyxl should not appear at beginning of lines in {output}'
-
-                if sys.version_info[:2] == (3, 12):
-                    assert 'openpyxl==2.5.4 [requires: et_xmlfile]' in output
-                else:
-                    assert 'openpyxl==2.5.4 [requires: et-xmlfile]' in output
+                assert openpyxl_dep is None, f'openpyxl should not appear at beginning of lines in {output}'s
+                assert 'openpyxl==2.5.4 [requires: et_xmlfile]' in output
             else:
                 dep_match = re.search(fr'^[ -]*{dep_name}==[\d.]+$',
                                       output,
