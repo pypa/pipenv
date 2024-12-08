3867ded57c18907107507e85c0b62afe6b314e34
Oz Tiram <oz.tiram@gmail.com>
2024-12-08 20:54:31 +0100

    chore: remove click echo from clear.py
    Signed-off-by: Oz Tiram <oz.tiram@gmail.com>

--- pipenv/routines/clear.py
+++ pipenv/routines/clear.py
@@ -1,14 +1,14 @@
 import shutil
 
 from pipenv import environments
+from pipenv.utils import console
 from pipenv.utils.funktools import handle_remove_readonly
-from pipenv.vendor import click
 
 
 def do_clear(project):
     from pipenv.patched.pip._internal import locations
 
-    click.secho("Clearing caches...", bold=True)
+    console.print("[bold]Clearing caches...[/bold]")
     try:
         shutil.rmtree(project.s.PIPENV_CACHE_DIR, onerror=handle_remove_readonly)
         # Other processes may be writing into this directory simultaneously.
