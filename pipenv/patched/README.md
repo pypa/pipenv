196522e49376da926712f726024ca00d15670ea1
Oz Tiram <oz.tiram@gmail.com>
2025-03-20 22:37:04 +0100

    fix: drop vendored safety
    Signed-off-by: Oz Tiram <oz.tiram@gmail.com>

--- pipenv/patched/README.md
+++ pipenv/patched/README.md
@@ -3,8 +3,6 @@
 - Pip is modified, to make it work with Pipenv's custom virtualenv locations.
 - Pip is modified, to make it work with pip-tool modifications.
 - Pip is modified, to make it resolve deep extras links.
-- Safety is hacked together to always work on any system.
-- TOML libraries are upgraded to... work.
 
 Don't touch.
 
--- pipenv/routines/check.py
+++ pipenv/routines/check.py
@@ -1,14 +1,17 @@
+import configparser
 import io
 import json
 import logging
 import os
+import subprocess as sp
+import shutil
 import sys
 import tempfile
 from contextlib import redirect_stderr, redirect_stdout
 from pathlib import Path
 
+
 from pipenv import pep508checker
-from pipenv.patched.safety.cli import cli
 from pipenv.utils.processes import run_command
 from pipenv.utils.project import ensure_project
 from pipenv.utils.shell import project_python
@@ -16,16 +19,13 @@ from pipenv.vendor import click, plette
 
 
 def build_safety_options(
-    audit_and_monitor=True,
     exit_code=True,
     output="screen",
     save_json="",
     policy_file="",
     safety_project=None,
-    temp_requirements_name="",
 ):
     options = [
-        "--audit-and-monitor" if audit_and_monitor else "--disable-audit-and-monitor",
         "--exit-code" if exit_code else "--continue-on-error",
     ]
     formats = {"full-report": "--full-report", "minimal": "--json"}
@@ -44,97 +44,14 @@ def build_safety_options(
     if safety_project:
         options.append(f"--project={safety_project}")
 
-    options.extend(["--file", temp_requirements_name])
-
     return options
 
 
-def run_pep508_check(project, system, python):
-    pep508checker_path = pep508checker.__file__.rstrip("cdo")
-    cmd = [project_python(project, system=system), Path(pep508checker_path).as_posix()]
-    c = run_command(cmd, is_verbose=project.s.is_verbose())
-
-    if c.returncode is not None:
-        try:
-            return json.loads(c.stdout.strip())
-        except json.JSONDecodeError:
-            click.echo(
-                f"Failed parsing pep508 results:\n{c.stdout.strip()}\n{c.stderr.strip()}",
-                err=True,
-            )
-            sys.exit(1)
-    return {}
-
-
-def check_pep508_requirements(project, results, quiet):
-    p = plette.Pipfile.load(open(project.pipfile_location))
-    p = plette.Lockfile.with_meta_from(p)
-    failed = False
-
-    for marker, specifier in p._data["_meta"]["requires"].items():
-        if marker in results:
-            if results[marker] != specifier:
-                failed = True
-                click.echo(
-                    "Specifier {} does not match {} ({})."
-                    "".format(
-                        click.style(marker, fg="green"),
-                        click.style(specifier, fg="cyan"),
-                        click.style(results[marker], fg="yellow"),
-                    ),
-                    err=True,
-                )
-
-    if failed:
-        click.secho("Failed!", fg="red", err=True)
-        sys.exit(1)
-    elif not quiet and not project.s.is_quiet():
-        click.secho("Passed!", fg="green")
-
-
-def get_requirements(project, use_installed, categories):
-    _cmd = [project_python(project, system=False)]
-    if use_installed:
-        return run_command(
-            _cmd + ["-m", "pip", "list", "--format=freeze"],
-            is_verbose=project.s.is_verbose(),
-        )
-    elif categories:
-        return run_command(
-            ["pipenv", "requirements", "--categories", categories],
-            is_verbose=project.s.is_verbose(),
-        )
-    else:
-        return run_command(["pipenv", "requirements"], is_verbose=project.s.is_verbose())
-
-
-def create_temp_requirements(project, requirements):
-    temp_requirements = tempfile.NamedTemporaryFile(
-        mode="w+",
-        prefix=f"{project.virtualenv_name}",
-        suffix="_requirements.txt",
-        delete=False,
-    )
-    temp_requirements.write(requirements.stdout.strip())
-    temp_requirements.close()
-    return temp_requirements
-
-
-def run_safety_check(cmd, quiet):
-    sys.argv = cmd[1:]
-
-    if quiet:
-        out = io.StringIO()
-        err = io.StringIO()
-        exit_code = 0
-        with redirect_stdout(out), redirect_stderr(err):
-            try:
-                cli(prog_name="pipenv")
-            except SystemExit as exit_signal:
-                exit_code = exit_signal.code
-        return out.getvalue(), err.getvalue(), exit_code
-    else:
-        cli(prog_name="pipenv")
+def run_safety_check(cmd, verbose):
+    if verbose:
+        click.echo(f"Running: {' '.join(cmd)}")
+    c = sp.run(cmd,capture_output=True)
+    return c.stdout, c.stderr, c.returncode
 
 
 def parse_safety_output(output, quiet):
@@ -175,6 +92,33 @@ def parse_safety_output(output, quiet):
     except json.JSONDecodeError:
         click.echo("Failed to parse Safety output.")
 
+def _get_safety():
+    return shutil.which("safety")
+
+def has_safey_auth_token() -> bool:
+    """"
+    Retrieve a token from the local authentication configuration.
+
+    This returns tokens saved in the local auth configuration.
+    There are two types of tokens: access_token and id_token
+
+    Args:
+        name (str): The name of the token to retrieve.
+
+    Returns:
+        Optional[str]: The token value, or None if not found.
+    """
+
+    authconfig = Path("~", ".safety", "auth.ini").expanduser() 
+    config = configparser.ConfigParser()
+    config.read(authconfig)
+    if 'auth' in config.sections() and 'access_token' in config['auth']:
+        value = config['auth']['access_token']
+        if value:
+            return True
+
+    return False
+    
 
 def do_check(
     project,
@@ -207,11 +151,14 @@ def do_check(
             pypi_mirror=pypi_mirror,
         )
 
-    if not quiet and not project.s.is_quiet():
-        click.secho("Checking PEP 508 requirements...", bold=True)
+    safety_path = _get_safety()
+    if not safety_path:
+        click.secho("Safety isn't installed. Please install safety on your system.", bold=True)
+        sys.exit(1)
 
-    results = run_pep508_check(project, system, python)
-    check_pep508_requirements(project, results, quiet)
+    if not has_safey_auth_token():
+        click.secho("Could not find saftey token. Use `safety check` to manually login to your account", bold=True)
+        sys.exit(1)
 
     if not project.lockfile_exists:
         return
@@ -239,36 +186,20 @@ def do_check(
                 err=True,
             )
 
-    requirements = get_requirements(project, use_installed, categories)
-    temp_requirements = create_temp_requirements(project, requirements)
-
     options = build_safety_options(
-        audit_and_monitor=audit_and_monitor,
         exit_code=exit_code,
         output=output,
         save_json=save_json,
         policy_file=policy_file,
         safety_project=safety_project,
-        temp_requirements_name=temp_requirements.name,
     )
 
-    safety_path = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), "patched", "safety"
-    )
-    cmd = [project_python(project, system=system), safety_path, "check"] + options
+    cmd = [safety_path, "check"] + options
 
     if db:
         if not quiet and not project.s.is_quiet():
             click.echo(f"Using {db} database")
         cmd.append(f"--db={db}")
-    elif key or project.s.PIPENV_PYUP_API_KEY:
-        cmd.append(f"--key={key or project.s.PIPENV_PYUP_API_KEY}")
-    else:
-        PIPENV_SAFETY_DB = (
-            "https://d2qjmgddvqvu75.cloudfront.net/aws/safety/pipenv/1.0.0/"
-        )
-        os.environ["SAFETY_ANNOUNCEMENTS_URL"] = f"{PIPENV_SAFETY_DB}announcements.json"
-        cmd.append(f"--db={PIPENV_SAFETY_DB}")
 
     if ignore:
         for cve in ignore:
@@ -277,14 +208,12 @@ def do_check(
     os.environ["SAFETY_CUSTOM_INTEGRATION"] = "True"
     os.environ["SAFETY_SOURCE"] = "pipenv"
     os.environ["SAFETY_PURE_YAML"] = "True"
-
-    output, error, exit_code = run_safety_check(cmd, quiet)
+    output, error, exit_code = run_safety_check(cmd, verbose)
 
     if quiet:
         parse_safety_output(output, quiet)
     else:
-        sys.stdout.write(output)
-        sys.stderr.write(error)
+        sys.stdout.write(output.decode())
+        sys.stderr.write(error.decode())
 
-    temp_requirements.unlink()
     sys.exit(exit_code)
