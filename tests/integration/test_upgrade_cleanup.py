f6c7ca92a29a50dc8d3d56f21d9e8bef5c901475
Matt Davis <matteius@gmail.com>
2025-04-25 03:27:03 -0400

    Cleanup unused dependencies

--- tests/integration/test_upgrade_cleanup.py
+++ tests/integration/test_upgrade_cleanup.py
@@ -10,9 +10,9 @@ from pipenv.utils.processes import subprocess_run
 
 @pytest.mark.upgrade
 @pytest.mark.cleanup
-def test_upgrade_removes_unused_dependencies(PipenvInstance):
+def test_upgrade_removes_unused_dependencies(pipenv_instance_pypi):
     """Test that `pipenv upgrade` removes dependencies that are no longer needed."""
-    with PipenvInstance(chdir=True) as p:
+    with pipenv_instance_pypi() as p:
         # Create a Pipfile with Django 3.2.10 (which depends on pytz)
         with open(p.pipfile_path, "w") as f:
             f.write("""
@@ -35,15 +35,11 @@ python_version = "3.11"
         assert c.returncode == 0
 
         # Verify pytz is in the lockfile
-        project = Project()
-        lockfile = project.lockfile()
-        assert "pytz" in lockfile["default"]
+        assert "pytz" in p.lockfile["default"]
 
         # Upgrade Django to 4.2.7 (which doesn't depend on pytz)
         c = p.pipenv("upgrade django==4.2.7")
         assert c.returncode == 0
 
         # Verify pytz is no longer in the lockfile
-        project = Project()
-        lockfile = project.lockfile()
-        assert "pytz" not in lockfile["default"]
+        assert "pytz" not in p.lockfile["default"]
