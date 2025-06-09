912b451487f1dfd0bdb30dd837462cda9d1a8060
Oz Tiram <oz.tiram@gmail.com>
2025-06-09 18:56:55 +0200

    chore: remove tomli search and replase from vendoring task
    Signed-off-by: Oz Tiram <oz.tiram@gmail.com>

--- tasks/vendoring/__init__.py
+++ tasks/vendoring/__init__.py
@@ -85,7 +85,6 @@ GLOBAL_REPLACEMENT = [
         "from pipenv.patched.pip._vendor.platformdirs import user_cache_dir",
     ),
     ("from distlib import", "from pipenv.patched.pip._vendor.distlib import"),
-    ("    import tomli", "    from pipenv.patched.pip._vendor import tomli"),
     (
         "from distlib.metadata import",
         "from pipenv.patched.pip._vendor.distlib.metadata import",
